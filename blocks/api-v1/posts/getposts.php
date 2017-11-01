73dbedf246c1d525c9ab099858365f08f7db98c1
rakibtg <rakibtg@gmail.com>
2017-11-01 21:52:56 +0600

    Renaming components -> blocks

---
+++ blocks/api-v1/posts/getposts.php
@@ -0,0 +1,9 @@
+<?php
+
+    $table   = 'posts';
+    $page = _get( 'page' );
+    $limit = _get( 'limit' );
+
+    if( $page == '' ) $page     = 1;
+    if( $limit == '' ) $limit   = 20;
+    echo $zink->db->table( $table )->orderBy( 'id', 'DESC' )->paginate( $page, $limit )->toJSON();
