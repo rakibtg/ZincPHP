5afd08ccb044fe7069c0d6958beec55a8a74f0c8
rakibtg <rakibtg@gmail.com>
2017-11-02 16:29:42 +0600

    Testing validator

--- blocks/test.php
+++ blocks/test.php
@@ -1,1 +1,21 @@
-scscs
+<?php
+  // Define rules.
+  $rules = [
+    'name' => [
+      'rules' => 'required|max:50',
+      'error' => [
+        'required' => 'Name is required.',
+        'max'      => 'Name is too big.',
+      ],
+      'value' => 'Kazi Mehedi Hasan'
+    ],
+    'email' => [
+      'rules' => 'required|email',
+      'error' => 'Email address is not valid',
+    ],
+    'age' => 'required|integer',
+    'username' => 'unique'
+  ];
+  // Start validating.
+  $validator->validate( $rules, 'get' );
+
