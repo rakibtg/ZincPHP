bb87e395ddbc91883bb0fd4f47432da35122f30d
rakibtg <rakibtg@gmail.com>
2017-11-03 12:54:56 +0600

    Testing validation logics

--- blocks/test.php
+++ blocks/test.php
@@ -2,29 +2,31 @@
   // Define rules.
   $rules = [
     'name' => [
-      'rules' => 'required|max:50',
+      'rules' => 'required|lengthMax:5|alphaNum|slug',
       'error' => [
         'required' => '{label} is required.',
-        'max'      => 'Name is too big.',
       ],
+      'value' => 'sjdhsuxbs xh'
     ],
     'email' => [
-      'rules' => 'required|email|different:full_name|length:5',
+      'rules' => 'required|email|different:full_name|length:5|contains:1ew|notContains:2ew|alpha',
       // 'error' => 'Email address is not valid',
-      'value' => 'asfasfas'
+      'value' => 'abcd'
     ],
     'age' => [
-      'rules' => 'required|integer',
-      'value' => 'twenty',
+      'rules' => 'required|integer|min:18|max:20|between:20:30',
+      'value' => 31,
     ],
     'username' => [
-      'rules' => 'required|accepted|array|numeric',
-      'value' => 'rakibtg'
+      'rules' => 'required|accepted|array|numeric|in:abc:xyz|notIn:sdsjd:sdsdsdw',
+      'value' => 'sdsjd'
     ],
     'dance' => [
-      'rules' => 'length:5',
-      'value' => 231241
-    ]
+      'rules' => 'length:5|lengthBetween:10:20|lengthMin:100',
+      'value' => 'yeahsdssadfasfadssgdsgfhghjfghdfgasdasddasdasdasdasdsfsdssadfasfadsgdsgasdasdasdasdsfghdfgasdasdaah'
+    ],
+    'myip' => 'ip',
+    'url' => 'url'
   ];
   // Start validating.
   $validator->validate( $rules, 'get' );
