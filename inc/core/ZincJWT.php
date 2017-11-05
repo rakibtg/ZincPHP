70a6d25a0bc9dceec78934864f419818c3fd41ba
rakibtg <rakibtg@gmail.com>
2017-11-06 00:37:29 +0600

    ZincJWT implemented

--- inc/core/ZincJWT.php
+++ inc/core/ZincJWT.php
@@ -6,55 +6,84 @@
    * 
    * @author  Hasan <rakibtg@gmail.com>
    * @link https://github.com/rakibtg/ZincPHP
-   */
+  */
 
-  require_once './__JWT__/JWT.php';
+  require_once '../inc/core/__JWT__/JWT.php';
 
   use \Firebase\JWT\JWT;
-
+  
   class ZincJWT {
+
+    /**
+     * Store the environment settings
+     * 
+     * @var array $env
+     */
+    private $env;
+
+    /**
+     * Boot the JWT class by storing the app environment.
+     * 
+     * @param   array $env Array that contains all the environment settings.
+     * @return  void 
+     */
+    function __construct( $env ) {
+      $this->env = $env;
+    }
 
     /**
      * Converts and signs a PHP object or array into a JWT string.
      *
-     * @param object|array  $payload    PHP object or array
-     * @param string        $key        The secret key.
-     *                                  If the algorithm used is asymmetric, this is the private key
-     * @param string        $returnType What to return on a fail attempt.
-     *                                  Supported returnables are 'boolean', 'error'
-     * @param string        $alg        The signing algorithm.
-     *                                  Supported algorithms are 'HS256', 'HS384', 'HS512' and 'RS256'
+     * @param object|array  $payload            PHP object or array
+     * @param string        $key                The secret key.
+     *                                          If the algorithm used is asymmetric, this is the private key
+     * @param string        $returnBoolOnFail   What to return on a fail attempt.
+     * @param string        $alg                The signing algorithm.
+     *                                          Supported algorithms are 'HS256', 'HS384', 'HS512' and 'RS256'
      * @param mixed         $keyId
-     * @param array         $head       An array with header elements to attach
+     * @param array         $head               An array with header elements to attach
      *
      * @return string A signed JWT
      *
      * @uses jsonEncode
      * @uses urlsafeB64Encode
      */
-    public function makeToken( $payload, $key, $returnType = 'boolean', $alg = 'HS256', $keyId = null, $head = null ) {
+    public function makeToken( $payload, $returnBoolOnFail = true, $alg = 'HS256', $keyId = null, $head = null ) {
       try {
-        return JWT::encode( $payload, $key, $alg = 'HS256', $keyId = null, $head = null );
+        return JWT::encode( $payload, $this->env->secret_keys, $alg = 'HS256', $keyId = null, $head = null );
       } catch( Exception $e ) {
-        if( $returnType == 'boolean' ) return false;
-        else if( $returnType == 'error' ) return $e;
+        if( $returnBoolOnFail == true ) return false;
+        else if( $returnBoolOnFail == false ) return $e;
         else return false;
       }
     }
 
-    public function getToken() {
-      // Get token from Authorization Header.
-      // Check validity and return.
-      /*
-  try {
-    $decoded = JWT::decode( $jwt, $key, [ 'HS256' ] );
-  } catch( Exception $e ) {
-    echo '<pre>';
-    echo 'Ops! Failed to authenticate.';
-    echo '</pre>';
-    exit();
-  }
-      */
+    /**
+     * Convert a valid token and return its payloads.
+     * @param string          $tokenString        JWT token
+     * @param boolean|array   $returnBoolOnFail   What type of data should return on a fail attempt.
+     * @param array           $alg                Set of supported algorithms.
+     * 
+     * @return boolean|array|void
+     */
+    public function getToken( $tokenString = false, $returnBoolOnFail = true, $alg = [ 'HS256' ] ) {
+      if( ! $tokenString ) {
+        if( isset( getallheaders()[ 'Authorization' ] ) ) {
+          $tokenString = getallheaders()[ 'Authorization' ];
+        } else {
+          if( $returnBoolOnFail == true ) return false;
+          else if( $returnBoolOnFail == false ) return 'Authorization token not found in the header.';
+          else return false;
+        }
+      }
+      try {
+        return JWT::decode( $tokenString, $this->env->secret_keys, $alg );
+      } catch( Exception $e ) {
+        if( $returnBoolOnFail == true ) return false;
+        else if( $returnBoolOnFail == false ) return $e;
+        else return false;
+      }
+
     }
 
   }
