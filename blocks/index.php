97835456f15ccc891a90a314d48a8a6ea0624170
rakibtg <rakibtg@gmail.com>
2017-11-08 00:56:52 +0600

    Default welcome page

---
+++ blocks/index.php
@@ -0,0 +1,8 @@
+<?php
+
+  // A sample welcome page.
+
+  _output( [
+    'zincPHP'   => 'Welcome!',
+    'toDo'      => 'To edit this page, go to \'index.php\' inside \'blocks\' directory',
+  ], 200, true );
