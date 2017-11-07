2a7c3d9b50b16247c6dd9c004a636c772d6083f7
rakibtg <rakibtg@gmail.com>
2017-11-07 23:51:31 +0600

    Optimized and clean 💖

--- system.php
+++ system.php
@@ -1,43 +1,19 @@
 <?php
-
+    require_once "../inc/core/Zinc.php";
     error_reporting( E_ALL );
     ini_set( 'display_errors', 1 );
 
-    $env = json_decode( file_get_contents( "../environment.json" ) );
-
-    require_once "../inc/core/Zinc.php";
-    $zink = new Zinc();
-
-    require_once "../inc/core/ZincValidator.php";
-    $validator = new ZincValidator();
-
-    require_once "../inc/core/ZincJWT.php";
-    $jwt = new ZincJWT( $env );
-
     // Setting JSON type globally.
     header( 'Content-Type: application/json; charset=utf-8' );
-
-    require_once "../inc/core_functions.php";
-
-    // Simple routing.
-    if( isset( $_GET[ 'route' ] ) ) {
-        $component = trim( $_GET[ 'route' ] );
-        if( empty( $component ) ) {
-            $component = 'index';
-        }
-    } else {
-        $component = 'index';
-    }
-
-    // Check if component exist.
-    $segments = '/'; 
-    // For security purposes recasting the url splitted by segments.
-    foreach( explode( '/', $component ) as $uri ) {
-        $segments = $segments . $uri . '/';
-    }
-    $component = '../blocks'.rtrim( $segments, '/' ).'.php';
-    if( file_exists( $component ) ) {
-        require_once $component;
-    } else {
-        zp\return_error( 'Controller not found.' );
-    }
+    
+    /**
+     * Instantiating Zinc core class.
+     * This will boot the framework, and will
+     * add other core libraries like validator, jwt, 
+     * database query builder etc with each block to be served.
+     * Dynamic routing will be handled from this class.
+     * 
+     * @param object $env The environment variables of this app.
+     * @return void
+     */
+    $zinc = new Zinc();
