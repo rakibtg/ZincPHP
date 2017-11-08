78999c07bb8e0addc9cc158eb6a06e700804432a
rakibtg <rakibtg@gmail.com>
2017-11-08 12:20:31 +0600

    Optimizing the system loader

--- system.php
+++ system.php
@@ -1,10 +1,5 @@
 <?php
     require_once "../inc/core/Zinc.php";
-    error_reporting( E_ALL );
-    ini_set( 'display_errors', 1 );
-
-    // Setting JSON type globally.
-    header( 'Content-Type: application/json; charset=utf-8' );
     
     /**
      * Instantiating Zinc core class.
@@ -13,7 +8,6 @@
      * database query builder etc with each block to be served.
      * Dynamic routing will be handled from this class.
      * 
-     * @param object $env The environment variables of this app.
      * @return void
      */
     $zinc = new Zinc();
