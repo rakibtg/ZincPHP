b80fe5b1ad9faedb4ffb7943b621124440c24fd6
rakibtg <rakibtg@gmail.com>
2017-11-08 12:13:53 +0600

    Documented all core functions

--- inc/core_functions.php
+++ inc/core_functions.php
@@ -2,25 +2,59 @@
 
     namespace zp;
 
+    /**
+     * Shortcut to print_r method.
+     *
+     * @param array $arr
+     *
+     * @return void
+     */
     function pr( $arr = [] ) {
         print_r( $arr );
     }
 
+    /**
+     * Checks if a variable exists, else
+     * returns null.
+     *
+     * @param any $str Any variable.
+     * @return string
+     */
     function strTrim( $str ) {
         if( ! isset( $str ) ) return '';
         return trim( $str );
     }
 
+    /**
+     * Get input data from GET requests.
+     *
+     * @param string $key Input field key name.
+     * @return string
+     */
     function get( $key ) {
         if( ! isset( $_GET[ $key ] ) ) return '';
         return strTrim( $_GET[ $key ] );
     }
 
+    /**
+     * Get input data form POST requests.
+     *
+     * @param string $key Input field key name.
+     * @return string
+     */
     function post( $key ) {
         if( ! isset( $_POST[ $key ] ) ) return '';
         return strTrim( $_POST[ $key ] );
     }
 
+    /**
+     * Converts data to json then print it.
+     *
+     * @param array|object|string   $data           Data to output
+     * @param integer               $resCode        Set the response status code
+     * @param boolean               $prettyPrint    Pretty prints the output JSON
+     * @return void
+     */
     function output( $data = [], $resCode = 200, $prettyPrint = false ) {
 
         // Setting the reponse code of the output.
@@ -37,6 +71,15 @@
         exit();
     }
 
+    /**
+     * Returns error with custom status response.
+     * If a error message was not provided then the default
+     * error message will be printed.
+     * 
+     * @param   string    $msg        Custom error message
+     * @param   integer   $status     HTTP status code, defualt is 404 (Not found)
+     * @return  void
+     */
     function return_error( $msg = false, $status = 404 ) {
         http_response_code( $status );
         if( ! $msg ) $msg = 'Unexpected error occured';
@@ -46,6 +89,12 @@
         exit();
     }
 
+    /**
+     * Returns the domain and with URI if provided.
+     * 
+     * @param   string  $uri    The URI to be append with the domain.
+     * @return  string
+     */
     function url( $uri = '/' ) {
         if( isset( $_SERVER[ "HTTPS" ] ) && ! empty( $_SERVER[ "HTTPS" ] ) && ( $_SERVER[ "HTTPS" ] != 'on' ) ) {
             $url = 'https://'.$_SERVER["SERVER_NAME"]; //https url
@@ -57,11 +106,23 @@
         else return $url . '/?route=' . $uri;
     }
 
-
+    /**
+     * The full path of the current route.
+     * 
+     * @return string
+     */
     function current_url() {
         return url() . $_SERVER["REQUEST_URI"];
     }
 
+    /**
+     * Generates slug of a string. Supports any languages.
+     * Try to avoid + as the seperator as this would break the slug.
+     * 
+     * @param   string $url         Plain text
+     * @param   string $seperator   The seperator of the slug
+     * @return  string
+     */
     function slug( $url = '', $seperator = '_' ) {
         $url = trim( $url );
         foreach( [ 
