4594bd3d4c0bb5d764d101b77ea48848eea540d6
rakibtg <rakibtg@gmail.com>
2017-11-17 21:03:14 +0600

    CLI color output for BASH environment

---
+++ inc/core/zinc_cli/cli_outputs.php
@@ -0,0 +1,34 @@
+<?php
+  namespace OuputCLI;
+  /**
+   * Outputs danger.
+   */
+  function danger( $msg ) {
+    if( PHP_OS != 'WINNT'  ) {
+      return "\033[31m$msg \033[0m";
+    } else {
+      return $msg;
+    }
+  }
+
+  /**
+   * Output success.
+   */
+  function success( $msg ) {
+    if( PHP_OS != 'WINNT'  ) {
+      return "\033[32m$msg \033[0m";
+    } else {
+      return $msg;
+    }
+  }
+
+  /**
+   * Output warning.
+   */
+  function warn( $msg ) {
+    if( PHP_OS != 'WINNT'  ) {
+      return "\033[33m$msg \033[0m";
+    } else {
+      return $msg;
+    }
+  }
