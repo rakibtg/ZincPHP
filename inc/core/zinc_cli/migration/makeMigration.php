2e468b78307eda32cfca9c963b1184d1b99b9159
rakibtg <rakibtg@gmail.com>
2017-11-19 16:54:39 +0600

    generate class name

--- inc/core/zinc_cli/migration/makeMigration.php
+++ inc/core/zinc_cli/migration/makeMigration.php
@@ -2,21 +2,34 @@
 
   /**
    * Creates a new migration file.
-   *
    */
 
-  if( ! isset( $argv[ 2 ] ) ) exit( "No migration name was found\n" );
-  if( empty( $argv[ 2 ] ) ) exit( "Migration name cant be empty\n" );
+  if( ! isset( $argv[ 2 ] ) ) exit( \OuputCLI\danger( "No migration name found\n" ) );
+  if( empty( $argv[ 2 ] ) ) exit( \OuputCLI\warn( "Migration name cant be empty\n" ) );
 
+  // Process migration name.
   $migrationName = trim( strtolower( $argv[ 2 ] ) );
-  $migrationName = str_replace( ' ', '_', $migrationName );
-  $migrationName = joinpaths( getcwd(), 'inc/migrations', $migrationName . '.php' );
-  if( ! file_exists( $migrationName ) ) {
+  $migrationName = ucfirst( str_replace( '-', ' ', $migrationName ) );
+  $migrationName = ucfirst( str_replace( '_', ' ', $migrationName ) );
+  $migrationName = explode( ' ', $migrationName );
+  foreach( $migrationName as $key => $mn ) {
+    $migrationName[ $key ] = trim( ucfirst( $mn ) );
+  }
+  $migrationName = implode( $migrationName );
+  $migrationFileName = joinpaths( getcwd(), 'inc/migrations', $migrationName . '.php' );
+  if( ! file_exists( $migrationFileName ) ) {
     // Copy new migration template to migrations directory.
-    if( copy( './inc/core/structures/new_migration.php.example', $migrationName ) ) {
-      print "Migration file($migrationName) was created\n";
+    $rawMigratable = file_get_contents( './inc/core/structures/new_migration.php.example' );
+    // Rename the class.
+    $rawMigratable = str_replace( '{{Migration}}', $migrationName, $rawMigratable );
+    // Save migration file.
+    if( file_put_contents( $migrationFileName, $rawMigratable ) ) {
+      print \OuputCLI\success( "Migration file($migrationName) was created\n" ) . "Migration File Path: " . $migrationFileName . "\n";
     }
+    // if( copy( './inc/core/structures/new_migration.php.example', $migrationName ) ) {
+    //   print \OuputCLI\success( "Migration file($migrationName) was created\n" );
+    // }
   } else {
-    print "Error: Migration file($migrationName) already exists!\n";
+    print \OuputCLI\danger( "Error: Migration file($migrationName) already exists!\n" ) . "Migration File Path: " . $migrationFileName . "\n";
   }
   exit(); // End of the zinc cli execution
