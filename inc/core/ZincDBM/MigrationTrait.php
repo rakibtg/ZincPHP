22fdc4e72c1cdf6d9ffe25b22b39fb8d6ba1f0e6
rakibtg <rakibtg@gmail.com>
2017-11-19 16:52:29 +0600

    Get all migration files

--- inc/core/ZincDBM/MigrationTrait.php
+++ inc/core/ZincDBM/MigrationTrait.php
@@ -2,14 +2,19 @@
 
   /**
    * Methods required for database migration.
-   * 
+   *
    */
   trait MigrationTrait {
 
     function listAllMigrations() {
       $migratableFiles = scandir( './inc/migrations' );
-      \zp\pr($migratableFiles);
-      return $this;
+      $toMigrate = [];
+      foreach( $migratableFiles as $mf ) {
+        if( $mf != '.' && $mf != '..' && substr( trim( $mf ), -4 ) == '.php' ) {
+          $toMigrate[] = './inc/migrations/' . $mf;
+        }
+      }
+      return $toMigrate;
     }
 
   }
