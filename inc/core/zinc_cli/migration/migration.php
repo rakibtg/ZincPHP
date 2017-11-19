98c25cebb1595bf923045b19ff66eea526b05a21
rakibtg <rakibtg@gmail.com>
2017-11-19 16:55:20 +0600

    Need more work here, instantiate eash migration file and execute

--- inc/core/zinc_cli/migration/migration.php
+++ inc/core/zinc_cli/migration/migration.php
@@ -1,7 +1,23 @@
 <?php
   require_once './inc/core/ZincDBM/ZincDBManager.php';
-  require_once './inc/migrations/goodjob.php';
+  // require_once './inc/migrations/goodjob.php';
   $zincDBManager = new ZincDBManager();
-  $g = new Migration( $zincDBManager );
-  $g->up();
+  // $g = new Migration( $zincDBManager );
+  // $g->up();
+  // list all php files.
+  // print_r( $zincDBManager->listAllMigrations() );
+  $migratable = $zincDBManager->listAllMigrations();
+
+  if( empty( $migratable ) ) exit( \OuputCLI\warn( "\n Nothing to migrate.\n\n" ) );
+
+  // Migrate each file.
+  foreach( $migratable as $mfile ) {
+    print \OuputCLI\success( "Migrating:" ) . basename( $mfile ) . "\n";
+    require_once $mfile;
+    $className = trim( rtrim( basename( $mfile ), '.php' ) );
+    $g = new $className( $zincDBManager );
+    $g->up();
+    unset($g);
+  }
+  print "\n";
   exit(); // End cli execution.
