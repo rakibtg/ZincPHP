edf429f5295d52e6b047b8f4e9f791c0c558a98e
rakibtg <rakibtg@gmail.com>
2017-12-08 02:55:39 +0600

    Static content for CLI

---
+++ app/core/zinc_cli/static/default_message.php
@@ -0,0 +1,11 @@
+<?php
+
+  \OuputCLI\nl();
+  echo \OuputCLI\success("  Welcome to ZincPHP!");
+  \OuputCLI\nl();
+  \OuputCLI\nl();
+  echo "  GitHub: https://github.com/rakibtg/ZincPHP";
+  \OuputCLI\nl();
+  echo "  Run 'php zinc help' to learn more :)";
+  \OuputCLI\nl();
+  \OuputCLI\nl();
