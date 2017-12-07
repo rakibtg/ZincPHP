b45f3af586185abc5eb77b9c40e8acbd302d0d66
rakibtg <rakibtg@gmail.com>
2017-12-08 02:56:33 +0600

    Moved few CLI functions to the helper

---
+++ app/core/zinc_cli/cli_helpers.php
@@ -0,0 +1,54 @@
+<?php
+  namespace OuputCLI;
+  /**
+   * Outputs danger.
+   */
+  function danger( $msg ) {
+    if( PHP_OS != 'WINNT'  ) {
+      return "\033[31m$msg \033[0m";
+    } else {
+      return $msg;
+    }
+  }
+
+  /**
+   * Output success.
+   */
+  function success( $msg ) {
+    if( PHP_OS != 'WINNT'  ) {
+      return "\033[32m$msg \033[0m";
+    } else {
+      return $msg;
+    }
+  }
+
+  /**
+   * Output warning.
+   */
+  function warn( $msg ) {
+    if( PHP_OS != 'WINNT'  ) {
+      return "\033[33m$msg \033[0m";
+    } else {
+      return $msg;
+    }
+  }
+
+  /**
+   * New line; Helpful for those who using ZSH.
+   */
+  function nl() {
+    echo "\n";
+  }
+
+  function trimds( $s ) {
+    return rtrim( $s, DIRECTORY_SEPARATOR );
+  }
+  function joinpaths() {
+    return implode( DIRECTORY_SEPARATOR, array_map( '\OuputCLI\trimds', func_get_args() ) );
+  }
+
+  if( ! file_exists( './app/environment.json' ) ) {
+    $env = false;
+  } else {
+    $env = json_decode( file_get_contents('./app/environment.json' ) );
+  }
