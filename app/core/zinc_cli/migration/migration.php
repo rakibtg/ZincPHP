4c8e113dd84c209adb0a62295b34b823118dcd17
rakibtg <rakibtg@gmail.com>
2017-12-13 00:17:38 +0600

    Migrator is now able to track each migration and tell if the action was a success or failed

--- app/core/zinc_cli/migration/migration.php
+++ app/core/zinc_cli/migration/migration.php
@@ -15,22 +15,38 @@
     echo \OuputCLI\warn( "Nothing to migrate." );
     echo \OuputCLI\nl();
     exit();
+  } else {
+    $nothingToMigrate = true;
   }
 
   // Migrate each file.
   foreach( $migratable as $mfile ) {
-    echo $mfile;
-    echo "\n";
+    // echo $mfile;
+    // echo "\n";
     
-    // if( $zincDBManager->addAsMigrated( $mfile ) ) {
-    //   print \OuputCLI\warn( "Trying to Migrate:" ) . basename( $mfile );
-    //   echo \OuputCLI\nl();
-    //   require_once $mfile;
-    //   $className = trim( rtrim( basename( $mfile ), '.php' ) );
-    //   $g = new $className( $zincDBManager );
-    //   $g->up();
-    //   unset($g);
-    // }
+    if( $zincDBManager->addAsMigrated( $mfile ) ) {
+      print \OuputCLI\warn( "Trying to Migrate:" ) . basename( $mfile );
+      // echo \OuputCLI\nl();
+      require_once $mfile;
+      $className = trim( rtrim( basename( $mfile ), '.php' ) );
+      $g = new $className( $zincDBManager );
+      if( $g->up() ) {
+        echo \OuputCLI\success( ' (Success)' );
+        echo \OuputCLI\nl();
+      } else {
+        echo \OuputCLI\danger( ' (Failed)' );
+        echo \OuputCLI\nl();
+      }
+      unset($g);
+      $nothingToMigrate = false;
+    }
   }
+
+  if( $nothingToMigrate ) {
+    echo \OuputCLI\warn( "Nothing to migrate." );
+    echo \OuputCLI\nl();
+    exit();
+  }
+
   echo \OuputCLI\nl();
   exit(); // End cli execution.
