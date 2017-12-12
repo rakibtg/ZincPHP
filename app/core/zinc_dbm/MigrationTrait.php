b6cdfeda773f65a10a1a53c8508b70205db8715d
rakibtg <rakibtg@gmail.com>
2017-12-12 23:58:51 +0600

    Adding support to keep track of new and old migrations

--- app/core/zinc_dbm/MigrationTrait.php
+++ app/core/zinc_dbm/MigrationTrait.php
@@ -6,6 +6,10 @@
    */
   trait MigrationTrait {
 
+    /**
+     * List all the migration files.
+     * 
+     */
     function listAllMigrations() {
       $migratableFiles = scandir( './app/migrations' );
       $toMigrate = [];
@@ -15,6 +19,43 @@
         }
       }
       return $toMigrate;
+    }
+
+    /**
+     * Read the migrationlist.json json file and return its value as php array.
+     * 
+     */
+    function readMigrationList() {
+      $migrationList = file_get_contents( './app/core/zinc_cli/migration/migrationlist.json' );
+      return json_decode( $migrationList, true );
+    }
+
+    /**
+     * This method would add a given migrated file name into the migrationlist.json file.
+     * 
+     */
+    function addAsMigrated( $filePath ) {
+      $migrationList = $this->readMigrationList();
+      $fileHash      = md5( trim( $filePath ) );
+      if( empty( $migrationList ) ) {
+        $migratable = true;
+        $migrationList = []; // Added an empty array to the migration list as it is empty.
+      } else {
+        $migratable = false;
+        // Check if this file exists in the list.
+        if( ! in_array( $fileHash, $migrationList ) ) {
+          $migratable = true;
+        } else {
+          $migratable = false;
+        }
+      }
+      if( $migratable == true ) {
+        // Append the new file name to the existing array.
+        $migrationList[] = $fileHash;
+        // Save this data to the json file.
+        file_put_contents( './app/core/zinc_cli/migration/migrationlist.json', json_encode( $migrationList ) );
+      }
+      return $migratable;
     }
 
   }
