6aeef115315d8085a35b0aa90abd4d95eb57004c
rakibtg <rakibtg@gmail.com>
2017-12-12 23:58:26 +0600

    Utilizing new DB Manager methods

--- app/core/zinc_cli/migration/migration.php
+++ app/core/zinc_cli/migration/migration.php
@@ -6,18 +6,31 @@
   // $g->up();
   // list all php files.
   // print_r( $zincDBManager->listAllMigrations() );
+  // print_r( $zincDBManager->readMigrationList() );
+  // print_r( $zincDBManager->addAsMigrated( 'family' ) );
+  // exit();
   $migratable = $zincDBManager->listAllMigrations();
 
-  if( empty( $migratable ) ) exit( \OuputCLI\warn( "\n Nothing to migrate.\n\n" ) );
+  if( empty( $migratable ) ) {
+    echo \OuputCLI\warn( "Nothing to migrate." );
+    echo \OuputCLI\nl();
+    exit();
+  }
 
   // Migrate each file.
   foreach( $migratable as $mfile ) {
-    print \OuputCLI\warn( "Trying to Migrate:" ) . basename( $mfile ) . "\n";
-    require_once $mfile;
-    $className = trim( rtrim( basename( $mfile ), '.php' ) );
-    $g = new $className( $zincDBManager );
-    $g->up();
-    unset($g);
+    echo $mfile;
+    echo "\n";
+    
+    // if( $zincDBManager->addAsMigrated( $mfile ) ) {
+    //   print \OuputCLI\warn( "Trying to Migrate:" ) . basename( $mfile );
+    //   echo \OuputCLI\nl();
+    //   require_once $mfile;
+    //   $className = trim( rtrim( basename( $mfile ), '.php' ) );
+    //   $g = new $className( $zincDBManager );
+    //   $g->up();
+    //   unset($g);
+    // }
   }
-  print "\n";
+  echo \OuputCLI\nl();
   exit(); // End cli execution.
