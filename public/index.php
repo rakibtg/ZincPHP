8690333d8a9922b3be195c69b552596d1a13dad3
rakibtg <rakibtg@gmail.com>
2018-01-05 00:58:11 +0600

    Moved boot file

--- public/index.php
+++ public/index.php
@@ -3,4 +3,4 @@
      * All public requests will be listened through this file.
      * This would also boot the app.
      */
-    require_once "../app/boot.php";
+    require_once "../app/core/boot.php";
