4f18b2db41c3b0138700e4be51e5fc90bb786607
rakibtg <rakibtg@gmail.com>
2018-01-13 01:01:19 +0600

    Handle signature invalid, before valid and expired exceptions

--- app/core/zinc_jwt/JWT.php
+++ app/core/zinc_jwt/JWT.php
@@ -1,6 +1,13 @@
 <?php
 
+
 namespace Firebase\JWT;
+
+require_once '../app/core/zinc_jwt/BeforeValidException.php';
+require_once '../app/core/zinc_jwt/ExpiredException.php';
+require_once '../app/core/zinc_jwt/SignatureInvalidException.php';
+
+
 use \DomainException;
 use \InvalidArgumentException;
 use \UnexpectedValueException;
