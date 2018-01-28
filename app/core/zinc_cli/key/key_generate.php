40ebc495232ae74aa840bc991ec14a3fec41324a
rakibtg <rakibtg@gmail.com>
2018-01-28 15:50:39 +0600

    New namespace

--- app/core/zinc_cli/key/key_generate.php
+++ app/core/zinc_cli/key/key_generate.php
@@ -2,5 +2,5 @@
   $env->secret_keys = bin2hex( random_bytes( 64 ) );
   file_put_contents( './app/environment.json', json_encode( $env, JSON_PRETTY_PRINT ) );
   echo "Secrent key has generated";
-  \OutputCLI\nl();
+  \ZincPHP\CLI\Helper\nl();
   exit();
