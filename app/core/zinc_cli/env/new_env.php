bb23ae5005ec9a5578f6b927f2c31ee574773181
rakibtg <rakibtg@gmail.com>
2018-01-28 15:50:26 +0600

    New namespace

--- app/core/zinc_cli/env/new_env.php
+++ app/core/zinc_cli/env/new_env.php
@@ -6,10 +6,10 @@
     $cont   = trim( fgets( $handle ) );
     if( strtolower( $cont ) == 'y' ) {
       echo "Overwriting environment.json file ...";
-      \OutputCLI\nl();
+      \ZincPHP\CLI\Helper\nl();
     } else {
       echo "Action canceled";
-      \OutputCLI\nl();
+      \ZincPHP\CLI\Helper\nl();
       exit();
     }
   }
@@ -17,6 +17,6 @@
   $default_env = json_decode( file_get_contents( './app/core/zinc_structures/environment.json.example' ) );
   $default_env->secret_keys = bin2hex( random_bytes( 64 ) );
   file_put_contents( './app/environment.json', json_encode( $default_env, JSON_PRETTY_PRINT ) );
-  echo \OutputCLI\success( "Environment document has created." );
-  \OutputCLI\nl();
+  echo \ZincPHP\CLI\Helper\success( "Environment document has created." );
+  \ZincPHP\CLI\Helper\nl();
   exit();
