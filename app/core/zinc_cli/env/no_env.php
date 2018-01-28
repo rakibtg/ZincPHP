d93a8854bd142cc14d11155c9c92c33e21d0e638
rakibtg <rakibtg@gmail.com>
2018-01-28 15:50:31 +0600

    New namespace

--- app/core/zinc_cli/env/no_env.php
+++ app/core/zinc_cli/env/no_env.php
@@ -1,7 +1,7 @@
 <?php
 
-echo \OutputCLI\danger( "ERROR" ) . "No environment document was found!";
-\OutputCLI\nl();
-echo \OutputCLI\warn( "TIPS " )."Run this command ".\OutputCLI\success( 'php zinc env:new' )."to generate a new env file.";
-\OutputCLI\nl();
+echo \ZincPHP\CLI\Helper\danger( "ERROR" ) . "No environment document was found!";
+\ZincPHP\CLI\Helper\nl();
+echo \ZincPHP\CLI\Helper\warn( "TIPS " )."Run this command ".\ZincPHP\CLI\Helper\success( 'php zinc env:new' )."to generate a new env file.";
+\ZincPHP\CLI\Helper\nl();
 exit();
