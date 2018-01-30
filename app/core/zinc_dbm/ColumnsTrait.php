6d699ea92375f590b4c044fbe19dacb9fd629154
rakibtg <rakibtg@gmail.com>
2018-01-30 16:42:42 +0600

    Foreign key and others

--- app/core/zinc_dbm/ColumnsTrait.php
+++ app/core/zinc_dbm/ColumnsTrait.php
@@ -1,69 +1,69 @@
 <?php
-  
+
   trait ColumnsTrait {
 
     // Auto-incrementing UNSIGNED BIGINT (primary key) equivalent column.
     function bigIncrements( $name ) {
-      $this->queryBody .= ', '.$name.' BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY ';
+      $this->queryBody .= ', `'.$name.'` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY ';
       return $this;
     }
 
     // BIGINT equivalent column.
     function bigInteger( $name ) {
-      $this->queryBody .= ', ' . $name . ' BIGINT ';
+      $this->queryBody .= ', `' . $name . '` BIGINT ';
       return $this;
     }
 
     // BLOB equivalent column.
     function binary( $name ) {
-      $this->queryBody .= ', ' . $name . ' BLOB ';
+      $this->queryBody .= ', `' . $name . '` BLOB ';
       return $this;
     }
 
     // BOOLEAN equivalent column.
     function boolean( $name ) {
-      $this->queryBody .= ', ' . $name . ' BOOLEAN ';
+      $this->queryBody .= ', `' . $name . '` BOOLEAN ';
       return $this;
     }
 
     // CHAR equivalent column with an optional length.
     function char( $name, $max = 250 ) {
-      $this->queryBody .= ', ' . $name . ' CHAR(' . $max . ') ';
+      $this->queryBody .= ', `' . $name . '` CHAR(' . $max . ') ';
       return $this;
     }
 
     // DATE equivalent column.
     function date( $name ) {
-      $this->queryBody .= ', ' . $name . ' DATE ';
+      $this->queryBody .= ', `' . $name . '` DATE ';
       return $this;
     }
 
     // DATETIME equivalent column.
     function dateTime( $name ) {
-      $this->queryBody .= ', ' . $name . ' DATETIME ';
+      $this->queryBody .= ', `' . $name . '` DATETIME ';
       return $this;
     }
 
     // DECIMAL equivalent column with a precision (total digits) and scale (decimal digits).
     function decimal( $name, $precision = 8, $scale = 2 ) {
-      $this->queryBody .= ', ' . $name . ' DECIMAL('.$precision.', '.$scale.') ';
+      $this->queryBody .= ', `' . $name . '` DECIMAL('.$precision.', '.$scale.') ';
       return $this;
     }
 
     // DOUBLE equivalent column with a precision (total digits) and scale (decimal digits).
     function double( $name, $precision = 8, $scale = 2 ) {
-      $this->queryBody .= ', ' . $name . ' DOUBLE('.$precision.', '.$scale.') ';
+      $this->queryBody .= ', `' . $name . '` DOUBLE('.$precision.', '.$scale.') ';
       return $this;
     }
 
     // ENUM equivalent column.
     function enum( $name, $list = [] ) {
       $_items = '';
-      return $this;
       foreach( $list as $i ) {
         $_items .= "'".$i."',";
       }
-      $this->queryBody .= ', ' . $name . ' ENUM( '.rtrim($_items, ',').' ) ';
+      $this->queryBody .= ', `' . $name . '` ENUM( '.rtrim($_items, ',').' ) ';
+      return $this;
     }
 
     // FLOAT equivalent column with a precision (total digits) and scale (decimal digits).
@@ -74,37 +74,37 @@
 
     // GEOMETRY equivalent column.
     function geometry( $name ) {
-      $this->queryBody .= ', ' . $name . ' GEOMETRY ';
+      $this->queryBody .= ', `' . $name . '` GEOMETRY ';
       return $this;
     }
 
     // GEOMETRYCOLLECTION equivalent column.
     function geometryCollection( $name ) {
-      $this->queryBody .= ', ' . $name . ' GEOMETRYCOLLECTION ';
+      $this->queryBody .= ', `' . $name . '` GEOMETRYCOLLECTION ';
       return $this;
     }
 
     // Auto-incrementing UNSIGNED INTEGER (primary key) equivalent column.
     function increments( $name ) {
-      $this->queryBody .= ', ' . $name . ' INTEGER UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY ';
+      $this->queryBody .= ', `' . $name . '` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY ';
       return $this;
     }
 
     // INTEGER equivalent column.
     function integer( $name ) {
-      $this->queryBody .= ', ' . $name . ' INTEGER ';
+      $this->queryBody .= ', `' . $name . '` INTEGER ';
       return $this;
     }
 
     // JSON equivalent column.
     function json( $name ) {
-      $this->queryBody .= ', ' . $name . ' JSON ';
+      $this->queryBody .= ', `' . $name . '` JSON ';
       return $this;
     }
 
     // JSONB equivalent column.
     function jsonb( $name ) {
-      $this->queryBody .= ', ' . $name . ' JSONB ';
+      $this->queryBody .= ', `' . $name . '` JSONB ';
       return $this;
     }
 
@@ -117,94 +117,124 @@
 
     // LINESTRING equivalent column.
     function lineString( $name ) {
-      $this->queryBody .= ', ' . $name . ' LINESTRING ';
+      $this->queryBody .= ', `' . $name . '` LINESTRING ';
       return $this;
     }
-    
+
     // LONGTEXT equivalent column.
     function longText( $name ) {
-      $this->queryBody .= ', ' . $name . ' LONGTEXT ';
+      $this->queryBody .= ', `' . $name . '` LONGTEXT ';
       return $this;
     }
 
     // Auto-incrementing UNSIGNED MEDIUMINT (primary key) equivalent colum.
     function mediumIncrements( $name ) {
-      $this->queryBody .= ', '.$name.' MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY ';
+      $this->queryBody .= ', `'.$name.'` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY ';
       return $this;
     }
-        
+
     // MEDIUMINT equivalent column.
     function mediumInteger( $name ) {
-      $this->queryBody .= ', ' . $name . ' MEDIUMINT ';
+      $this->queryBody .= ', `' . $name . '` MEDIUMINT ';
       return $this;
     }
-     
+
     // MEDIUMTEXT equivalent column.
     function mediumText( $name ) {
-      $this->queryBody .= ', ' . $name . ' MEDIUMTEXT ';
+      $this->queryBody .= ', `' . $name . '` MEDIUMTEXT ';
       return $this;
     }
 
     // Auto-incrementing UNSIGNED SMALLINT (primary key) equivalent column.
     function smallIncrements( $name ) {
-      $this->queryBody .= ', '.$name.' SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY ';
+      $this->queryBody .= ', `'.$name.'` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY ';
       return $this;
     }
 
     // SMALLINT equivalent column.
     function smallInteger( $name ) {
-      $this->queryBody .= ', ' . $name . ' SMALLINT ';
+      $this->queryBody .= ', `' . $name . '` SMALLINT ';
       return $this;
     }
 
     // VARCHAR equivalent column with a optional length.
     function string( $name, $max = 250 ) {
-      $this->queryBody .= ', ' . $name . ' VARCHAR('.$max.') ';
+      $this->queryBody .= ', `' . $name . '` VARCHAR('.$max.') ';
       return $this;
     }
 
     // TEXT equivalent column.
     function text( $name ) {
-      $this->queryBody .= ', ' . $name . ' TEXT ';
+      $this->queryBody .= ', `' . $name . '` TEXT ';
       return $this;
     }
 
     // TIME equivalent column.
     function time( $name ) {
-      $this->queryBody .= ', ' . $name . ' TIME ';
+      $this->queryBody .= ', `' . $name . '` TIME ';
       return $this;
     }
 
     // TIMESTAMP equivalent column.
     function timestamp( $name ) {
-      $this->queryBody .= ', ' . $name . ' TIMESTAMP ';
+      $this->queryBody .= ', `' . $name . '` TIMESTAMP ';
       return $this;
     }
 
     // Auto-incrementing UNSIGNED TINYINT (primary key) equivalent column.
     function tinyIncrements( $name ) {
-      $this->queryBody .= ', '.$name.'TINYINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY ';
+      $this->queryBody .= ', `'.$name.'`TINYINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY ';
       return $this;
     }
 
     // TINYINT equivalent column.
     function tinyInteger( $name ) {
-      $this->queryBody .= ', ' . $name . ' TINYINT ';
+      $this->queryBody .= ', `' . $name . '` TINYINT ';
       return $this;
     }
 
     // UUID equivalent column.
     function uuid( $name ) {
-      $this->queryBody .= ', ' . $name . ' UUID ';
+      $this->queryBody .= ', `' . $name . '` UUID ';
+      return $this;
+    }
+
+    /**
+     * Add a foreign key relation between current tables column to another column primary key.
+     * @param  string  $columnName        Column that has the relation.
+     * @param  string  $referencedColumn  Dependent column.
+     * @param  string  $referencedTable   Dependent table.
+     * @return object  ...                Current object.
+     *
+     */
+    function foreignKey( $columnName, $referencedTable, $referencedColumn = false ) {
+      $this->queryBody .= ', FOREIGN KEY (`'.$columnName.'`) REFERENCES `'.$referencedTable.'`(`'.$referencedColumn.'`)';
+      return $this;
+    }
+
+    /**
+     * Add a foreign key relation between existing tables column to another column primary key.
+     * @param  string  $columnName        Column that has the relation.
+     * @param  string  $referencedColumn  Dependent column.
+     * @param  string  $referencedTable   Dependent table.
+     * @return object  ...                Current object.
+     *
+     */
+    function addNewForeignKey( $columnName, $referencedTable, $referencedColumn = false ) {
+      $this->rawQuery .= '
+        ALTER TABLE `'.$this->tableName.'`
+        ADD FOREIGN KEY (`'.$columnName.'`)
+        REFERENCES `'.$referencedTable.'`(`'.$referencedColumn.'`);
+      ';
       return $this;
     }
 
     /**
      * Add an index for one or more than one columns.
-     * 
+     *
      * @param   string|array  $columns    If string then it is a single column, for
      *                                    multiple coloumns we can use an array.
-     * @param   string        $indexName  Index name, if not provided then the first column 
+     * @param   string        $indexName  Index name, if not provided then the first column
      *                                    name will be the default name for the index.
      * @return  object        ...         Current object.
      */
@@ -216,10 +246,10 @@
 
     /**
      * Add an index to an existing table for one or more than one columns.
-     * 
+     *
      * @param   string|array  $columns    If string then it is a single column, for
      *                                    multiple coloumns we can use an array.
-     * @param   string        $indexName  Index name, if not provided then the first column 
+     * @param   string        $indexName  Index name, if not provided then the first column
      *                                    name will be the default name for the index.
      * @return  object        ...         Current object.
      */
@@ -235,7 +265,7 @@
 
     /**
      * Add an index to an existing table for one or more than one columns.
-     * 
+     *
      * @param   string   $indexName  Index name to be dropped.
      * @return  object   ...         Current object.
      */
@@ -248,7 +278,7 @@
 
     /**
      * Renames a column.
-     * 
+     *
      * @param   string   $oldName    Existing name of the column.
      * @param   string   $newName    New name for the column(optional)
      * @param   string   $dataType   Datatype for the column.
@@ -268,8 +298,16 @@
     }
 
     /**
+     * Alias to alterColumn() method.
+     *
+     */
+    function renameColumn( $oldName, $newName = false, $dataType, $dataLimit = false ) {
+      return $this->alterColumn( $oldName, $newName = false, $dataType, $dataLimit = false );
+    }
+
+    /**
      * Make a RAW query.
-     * 
+     *
      * @param   string   $query   RAW query to execute for a column.
      * @return  object   ...      Current object.
      */
@@ -280,7 +318,7 @@
 
     /**
      * Drop a column.
-     * 
+     *
      * @param   string  $columnName   The column name need to drop.
      * @return  object  ...           Current object.
      */
@@ -293,7 +331,7 @@
 
     /**
      * Add a new column.
-     * 
+     *
      * @return  object  ...  Current object.
      */
     function addNewColumn () {
