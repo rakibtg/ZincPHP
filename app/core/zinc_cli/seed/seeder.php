f14b8a6105ad3dd63b6d73b469de2b80a1d7f83a
rakibtg <rakibtg@gmail.com>
2018-02-01 12:51:54 +0600

    Added comments

--- app/core/zinc_cli/seed/seeder.php
+++ app/core/zinc_cli/seed/seeder.php
@@ -1,5 +1,11 @@
 <?php
 
-  require_once './app/core/zinc_dbm/ZincDBManager.php';
+  /*
 
+    This PHP file will import the ZincDBManager class and will
+    execute the seed command. The seed command will run the seeder.
+
+  */
+
+  require_once './app/core/zinc_dbm/ZincDBManager.php';
   ( new ZincDBManager() )->seed( $argv );
