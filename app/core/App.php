2d51dc26e9d361cd31ef8a576387b9c2161ac08a
tithi021 <afroza021@gmail.com>
2018-02-07 13:25:08 +0600

    Singleton static method for validation, JWT, DB and requests

--- app/core/App.php
+++ app/core/App.php
@@ -1,30 +1,42 @@
 <?php
- 
-  require_once '../app/core/Traits/HelperTraits.php';
-  
-  class App {
- 
-    /**
-     * Alias to Validator class validate() method.
-     * 
-     */
-    static public function validate ( $toValid = [], $queryStringType = 'get', $exitAfterExecution = true ) {
-      return ( new ZincValidator() )->validate( $toValid, $queryStringType, $exitAfterExecution );
-    }
- 
-    /**
-     * Get all the environments data.
-     * 
-     * @return array App settings; JSON document as array.
-     */
-    static public function environment() {
-      // Import and set environment variables from environment document.
-      if( ! file_exists( '../app/environment.json' ) ) {
-        \zp\response_error( 'Environment document was not found! Run \'php zinc env:new\' command to create a new environment document.' );
-        exit();
-      }
-      // Set environment settings
-      return json_decode( file_get_contents( '../app/environment.json' ) );
-    }
- 
-  }
+     
+     require_once '../app/core/Traits/HelperTraits.php';
+     
+     class App {
+    
+       /**
+        * Alias to Validator class validate() method.
+        * 
+        */
+       static public function validate ( $toValid = [], $queryStringType = 'get', $exitAfterExecution = true ) {
+         return ( new ZincValidator() )->validate( $toValid, $queryStringType, $exitAfterExecution );
+       }
+    
+       /**
+        * Alias to ZincJWT class.
+        * 
+        */
+       static public function jwt() {
+         return new ZincJWT( App::environment() );
+       }
+    
+       /**
+        * Alias to send a request.
+        * 
+        */
+       static public function makeRequest() {
+         return new ZincHTTP;
+       }
+    
+       /**
+        * Alias to db.
+        * 
+        */
+       static public function db () {
+         $env = App::environment();
+         if( ! empty( $env->database ) ) {
+           return DB::getInstance( $env );
+         }
+       }
+    
+     }
