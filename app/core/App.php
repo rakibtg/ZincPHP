5ed3e16420a23ff97a93a6b6601fd592f74f0e2b
Kazi Mehedi Hasan <rakibtg@gmail.com>
2018-02-09 19:03:42 +0600

    fixed static method declaration

--- app/core/App.php
+++ app/core/App.php
@@ -1,42 +1,44 @@
 <?php
-     
-     require_once '../app/core/Traits/HelperTraits.php';
-     
-     class App {
-    
-       /**
-        * Alias to Validator class validate() method.
-        * 
-        */
-       static public function validate ( $toValid = [], $queryStringType = 'get', $exitAfterExecution = true ) {
-         return ( new ZincValidator() )->validate( $toValid, $queryStringType, $exitAfterExecution );
-       }
-    
-       /**
-        * Alias to ZincJWT class.
-        * 
-        */
-       static public function jwt() {
-         return new ZincJWT( App::environment() );
-       }
-    
-       /**
-        * Alias to send a request.
-        * 
-        */
-       static public function makeRequest() {
-         return new ZincHTTP;
-       }
-    
-       /**
-        * Alias to db.
-        * 
-        */
-       static public function db () {
-         $env = App::environment();
-         if( ! empty( $env->database ) ) {
-           return DB::getInstance( $env );
-         }
-       }
-    
-     }
+
+  require_once __DIR__ . '/app_traits/HelperTraits.php';
+
+  class App {
+
+    use HelperTraits;
+
+    /**
+    * Alias to Validator class validate() method.
+    * 
+    */
+    public static function validate ( $toValid = [], $queryStringType = 'get', $exitAfterExecution = true ) {
+      return ( new ZincValidator() )->validate( $toValid, $queryStringType, $exitAfterExecution );
+    }
+
+    /**
+    * Alias to ZincJWT class.
+    * 
+    */
+    public static function jwt() {
+      return new ZincJWT( App::environment() );
+    }
+
+    /**
+    * Alias to send a request.
+    * 
+    */
+    public static function makeRequest() {
+      return new ZincHTTP;
+    }
+
+    /**
+    * Alias to db.
+    * 
+    */
+    public static function db () {
+      $env = App::environment();
+      if( ! empty( $env->database ) ) {
+        return DB::getInstance( $env );
+      }
+    }
+
+  }
