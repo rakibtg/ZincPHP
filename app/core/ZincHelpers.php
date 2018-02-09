0860c231c42665db62469c3f2d8ea1dcc7887cf5
rakibtg <rakibtg@gmail.com>
2018-02-10 00:14:16 +0600

    Migrated all helpers to their traits

--- app/core/ZincHelpers.php
+++ app/core/ZincHelpers.php
@@ -37,173 +37,173 @@
     //     return trim( strtolower( $_SERVER[ 'REQUEST_METHOD' ] ) );
     // }
 
-    /**
-     * Get input data from GET requests.
-     *
-     * @param string $key Input field key name.
-     * @return string
-     */
-    function get( $key = false ) {
-        if( $key === false ) {
-            return $_GET;
-        } else {
-            if ( isset( $_GET[ $key ] ) ) {
-                return strTrim( $_GET[ $key ] );
-            } else {
-                return '';
-            }
-        }
-    }
+    // /**
+    //  * Get input data from GET requests.
+    //  *
+    //  * @param string $key Input field key name.
+    //  * @return string
+    //  */
+    // function get( $key = false ) {
+    //     if( $key === false ) {
+    //         return $_GET;
+    //     } else {
+    //         if ( isset( $_GET[ $key ] ) ) {
+    //             return strTrim( $_GET[ $key ] );
+    //         } else {
+    //             return '';
+    //         }
+    //     }
+    // }
 
-    /**
-     * Get input data form POST requests.
-     *
-     * @param string $key Input field key name.
-     * @return string
-     */
-    function post( $key = false ) {
-        if( $key === false ) {
-            return $_POST;
-        } else {
-            if ( isset( $_POST[ $key ] ) ) {
-                return strTrim( $_POST[ $key ] );
-            } else {
-                return '';
-            }
-        }
-    }
+    // /**
+    //  * Get input data form POST requests.
+    //  *
+    //  * @param string $key Input field key name.
+    //  * @return string
+    //  */
+    // function post( $key = false ) {
+    //     if( $key === false ) {
+    //         return $_POST;
+    //     } else {
+    //         if ( isset( $_POST[ $key ] ) ) {
+    //             return strTrim( $_POST[ $key ] );
+    //         } else {
+    //             return '';
+    //         }
+    //     }
+    // }
 
-    /**
-     * Get input data form PUT requests.
-     *
-     * @param string $key Input field key name.
-     * @return string
-     */
-    function put( $key ) {
-        if( \zp\requestType() === 'put' ) {
-            if( ! isset( $_POST[ $key ] ) ) return '';
-            return strTrim( $_POST[ $key ] );
-        }
-        return '';
-    }
-
-
-    /**
-     * Get input data form DELETE requests.
-     *
-     * @param string $key Input field key name.
-     * @return string
-     */
-    function delete( $key ) {
-        if( \zp\requestType() === 'delete' ) {
-            if( ! isset( $_POST[ $key ] ) ) return '';
-            return strTrim( $_POST[ $key ] );
-        }
-        return '';
-    }
+    // /**
+    //  * Get input data form PUT requests.
+    //  *
+    //  * @param string $key Input field key name.
+    //  * @return string
+    //  */
+    // function put( $key ) {
+    //     if( \zp\requestType() === 'put' ) {
+    //         if( ! isset( $_POST[ $key ] ) ) return '';
+    //         return strTrim( $_POST[ $key ] );
+    //     }
+    //     return '';
+    // }
 
 
-    /**
-     * Get input data form COPY requests.
-     *
-     * @param string $key Input field key name.
-     * @return string
-     */
-    function copy( $key ) {
-        if( \zp\requestType() === 'copy' ) {
-            if( ! isset( $_POST[ $key ] ) ) return '';
-            return strTrim( $_POST[ $key ] );
-        }
-        return '';
-    }
+    // /**
+    //  * Get input data form DELETE requests.
+    //  *
+    //  * @param string $key Input field key name.
+    //  * @return string
+    //  */
+    // function delete( $key ) {
+    //     if( \zp\requestType() === 'delete' ) {
+    //         if( ! isset( $_POST[ $key ] ) ) return '';
+    //         return strTrim( $_POST[ $key ] );
+    //     }
+    //     return '';
+    // }
 
 
-    /**
-     * Get input data form OPTIONS requests.
-     *
-     * @param string $key Input field key name.
-     * @return string
-     */
-    function options( $key ) {
-        if( \zp\requestType() === 'options' ) {
-            if( ! isset( $_POST[ $key ] ) ) return '';
-            return strTrim( $_POST[ $key ] );
-        }
-        return '';
-    }    
+    // /**
+    //  * Get input data form COPY requests.
+    //  *
+    //  * @param string $key Input field key name.
+    //  * @return string
+    //  */
+    // function copy( $key ) {
+    //     if( \zp\requestType() === 'copy' ) {
+    //         if( ! isset( $_POST[ $key ] ) ) return '';
+    //         return strTrim( $_POST[ $key ] );
+    //     }
+    //     return '';
+    // }
 
 
-    /**
-     * Get input data form PROPFIND requests.
-     *
-     * @param string $key Input field key name.
-     * @return string
-     */
-    function propfind( $key ) {
-        if( \zp\requestType() === 'propfind' ) {
-            if( ! isset( $_POST[ $key ] ) ) return '';
-            return strTrim( $_POST[ $key ] );
-        }
-        return '';
-    }   
+    // /**
+    //  * Get input data form OPTIONS requests.
+    //  *
+    //  * @param string $key Input field key name.
+    //  * @return string
+    //  */
+    // function options( $key ) {
+    //     if( \zp\requestType() === 'options' ) {
+    //         if( ! isset( $_POST[ $key ] ) ) return '';
+    //         return strTrim( $_POST[ $key ] );
+    //     }
+    //     return '';
+    // }    
+
+
+    // /**
+    //  * Get input data form PROPFIND requests.
+    //  *
+    //  * @param string $key Input field key name.
+    //  * @return string
+    //  */
+    // function propfind( $key ) {
+    //     if( \zp\requestType() === 'propfind' ) {
+    //         if( ! isset( $_POST[ $key ] ) ) return '';
+    //         return strTrim( $_POST[ $key ] );
+    //     }
+    //     return '';
+    // }   
     
 
-    /**
-     * Get input data form PATCH requests.
-     *
-     * @param string $key Input field key name.
-     * @return string
-     */
-    function patch( $key ) {
-        if( \zp\requestType() === 'patch' ) {
-            if( ! isset( $_POST[ $key ] ) ) return '';
-            return strTrim( $_POST[ $key ] );
-        }
-        return '';
-    }   
+    // /**
+    //  * Get input data form PATCH requests.
+    //  *
+    //  * @param string $key Input field key name.
+    //  * @return string
+    //  */
+    // function patch( $key ) {
+    //     if( \zp\requestType() === 'patch' ) {
+    //         if( ! isset( $_POST[ $key ] ) ) return '';
+    //         return strTrim( $_POST[ $key ] );
+    //     }
+    //     return '';
+    // }   
 
 
-    /**
-     * Converts data to json then print it.
-     *
-     * @param array|object|string   $data           Data to output
-     * @param integer               $responseStatus Set the response status code
-     * @param boolean               $prettyPrint    Pretty prints the output JSON
-     * @return void
-     */
-    function response( $data = [], $responseStatus = 200, $prettyPrint = false ) {
+    // /**
+    //  * Converts data to json then print it.
+    //  *
+    //  * @param array|object|string   $data           Data to output
+    //  * @param integer               $responseStatus Set the response status code
+    //  * @param boolean               $prettyPrint    Pretty prints the output JSON
+    //  * @return void
+    //  */
+    // function response( $data = [], $responseStatus = 200, $prettyPrint = false ) {
 
-        // Setting the reponse code of the output.
-        http_response_code( $responseStatus );
+    //     // Setting the reponse code of the output.
+    //     http_response_code( $responseStatus );
 
-        if( empty( $data ) ) {
-            echo json_encode( [] );
-        } else if ( is_array( $data ) ) {
-            if( $prettyPrint ) echo json_encode( $data, JSON_PRETTY_PRINT );
-            else echo json_encode( $data );
-        } else {
-            echo json_encode( [ $data ] );
-        }
-        exit();
-    }
+    //     if( empty( $data ) ) {
+    //         echo json_encode( [] );
+    //     } else if ( is_array( $data ) ) {
+    //         if( $prettyPrint ) echo json_encode( $data, JSON_PRETTY_PRINT );
+    //         else echo json_encode( $data );
+    //     } else {
+    //         echo json_encode( [ $data ] );
+    //     }
+    //     exit();
+    // }
 
-    /**
-     * Returns error with custom status response.
-     * If a error message was not provided then the default
-     * error message will be printed.
-     *
-     * @param   string    $msg        Custom error message
-     * @param   integer   $status     HTTP status code, defualt is 404 (Not found)
-     * @return  void
-     */
-    function response_error( $msg = false, $status = 404 ) {
-        http_response_code( $status );
-        if( ! $msg ) $msg = 'Unexpected error occured';
-        echo json_encode( [
-            'status' => $msg,
-        ] );
-        exit();
-    }
+    // /**
+    //  * Returns error with custom status response.
+    //  * If a error message was not provided then the default
+    //  * error message will be printed.
+    //  *
+    //  * @param   string    $msg        Custom error message
+    //  * @param   integer   $status     HTTP status code, defualt is 404 (Not found)
+    //  * @return  void
+    //  */
+    // function response_error( $msg = false, $status = 404 ) {
+    //     http_response_code( $status );
+    //     if( ! $msg ) $msg = 'Unexpected error occured';
+    //     echo json_encode( [
+    //         'status' => $msg,
+    //     ] );
+    //     exit();
+    // }
 
     // /**
     //  * Returns the domain and with URI if provided.
