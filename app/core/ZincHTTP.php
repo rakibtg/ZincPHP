6ec7f708d33396b0ba06bb30f0684a873f837a15
rakibtg <rakibtg@gmail.com>
2018-02-11 21:13:04 +0600

    Implemented all supported HTTP methods, code refactored and removed static methods.

--- app/core/ZincHTTP.php
+++ app/core/ZincHTTP.php
@@ -11,7 +11,7 @@ class ZincHTTP {
    * @param       array $params
    * @return      HTTP-Response body or an empty string if the request fails or is empty
    */
-  public static function HTTPGet( $url, array $params ) {
+  public function HTTPGet( $url, $params = [] ) {
     $query = http_build_query( $params ); 
     $ch    = curl_init( $url . '?' . $query );
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
@@ -27,7 +27,7 @@ class ZincHTTP {
    * @param       array $params
    * @return      HTTP-Response body or an empty string if the request fails or is empty
    */
-  public static function HTTPPost( $url, array $params ) {
+  public function HTTPPost( $url, $params = [] ) {
     $query = http_build_query( $params );
     $ch    = curl_init();
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
@@ -41,22 +41,32 @@ class ZincHTTP {
   }
 
   /**
-   * Make HTTP-PUT call
+   * Formula to make any HTTP-POST requests.
    * @param       $url
-   * @param       array $params
-   * @return      HTTP-Response body or an empty string if the request fails or is empty
+   * @param       array   $params
+   * @param       string  The method name.
+   * @return      HTTP-Response body or an empty string if the request fails or is empty.
    */
-  public static function HTTPPut( $url, array $params ) {
+  private function makeRequestsFormula( $url, $params, $method ) {
     $query = \http_build_query( $params );
     $ch    = \curl_init();
     \curl_setopt( $ch, \CURLOPT_RETURNTRANSFER, true );
     \curl_setopt( $ch, \CURLOPT_HEADER, false );
     \curl_setopt( $ch, \CURLOPT_URL, $url );
-    \curl_setopt( $ch, \CURLOPT_CUSTOMREQUEST, 'PUT' );
+    \curl_setopt( $ch, \CURLOPT_CUSTOMREQUEST, $method );
     \curl_setopt( $ch, \CURLOPT_POSTFIELDS, $query );
     $response = \curl_exec( $ch );
     \curl_close( $ch );
     return $response;
+  }
+  /**
+   * Make HTTP-PUT call
+   * @param       $url
+   * @param       array $params Parameters with the request.
+   * @return      HTTP-Response body or an empty string if the request fails or is empty
+   */
+  public function HTTPPut( $url, $params = [] ) {
+    return $this->makeRequestsFormula( $url, $params, 'PUT' );
   }
 
   /**
@@ -65,16 +75,48 @@ class ZincHTTP {
    * @param    array $params
    * @return   HTTP-Response body or an empty string if the request fails or is empty
    */
-  public static function HTTPDelete( $url, array $params ) {
-    $query = \http_build_query( $params );
-    $ch    = \curl_init();
-    \curl_setopt( $ch, \CURLOPT_RETURNTRANSFER, true );
-    \curl_setopt( $ch, \CURLOPT_HEADER, false );
-    \curl_setopt( $ch, \CURLOPT_URL, $url );
-    \curl_setopt( $ch, \CURLOPT_CUSTOMREQUEST, 'DELETE' );
-    \curl_setopt( $ch, \CURLOPT_POSTFIELDS, $query );
-    $response = \curl_exec( $ch );
-    \curl_close( $ch );
-    return $response;
+  public function HTTPDelete( $url, $params = [] ) {
+    return $this->makeRequestsFormula( $url, $params, 'DELETE' );
   }
+
+  /**
+   * Make HTTP-COPY call
+   * @param    $url
+   * @param    array $params
+   * @return   HTTP-Response body or an empty string if the request fails or is empty
+   */
+  public function HTTPCopy( $url, $params = [] ) {
+    return $this->makeRequestsFormula( $url, $params, 'COPY' );
+  }
+
+  /**
+   * Make HTTP-OPTIONS call
+   * @param    $url
+   * @param    array $params
+   * @return   HTTP-Response body or an empty string if the request fails or is empty
+   */
+  public function HTTPOptions( $url, $params = [] ) {
+    return $this->makeRequestsFormula( $url, $params, 'OPTIONS' );
+  }
+
+  /**
+   * Make HTTP-PATCH call
+   * @param    $url
+   * @param    array $params
+   * @return   HTTP-Response body or an empty string if the request fails or is empty
+   */
+  public function HTTPPatch( $url, $params = [] ) {
+    return $this->makeRequestsFormula( $url, $params, 'PATCH' );
+  }
+
+  /**
+   * Make HTTP-PROPFIND call
+   * @param    $url
+   * @param    array $params
+   * @return   HTTP-Response body or an empty string if the request fails or is empty
+   */
+  public function HTTPPropfind( $url, $params = [] ) {
+    return $this->makeRequestsFormula( $url, $params, 'PROPFIND' );
+  }
+
 }
