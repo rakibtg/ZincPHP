57ae62d2b48092faf9223cf05dab34e810d0d4c0
rakibtg <rakibtg@gmail.com>
2018-02-18 00:34:03 +0600

    run test for blocks

--- app/core/zincphp_tester/runTest.php
+++ app/core/zincphp_tester/runTest.php
@@ -1,10 +1,21 @@
 <?php
 
   require_once __DIR__ . '/ZincTester.php';
+  require_once __DIR__ . '/BlockTester.php';
+
   $tester = new ZincTester();
+
+  // // Test test started.
+  // require_once '/home/rakibtg/Projects/ZincPHP/blocks/test-make-request/tests/put.test-make-request.test.php';
+  // $blockTester = new Puttestmakerequest();
+  // $blockTester->makeTest();
+  // $blockTester->runTest();
+  // Test test end.
+  // exit();
+
   $testables = $tester->getTestDirectories();
   if ( $tester->blocksCount > 0 ) {
-    echo \ZincPHP\CLI\Helper\success( $tester->blocksCount . " blocks and " . ( $tester->testFilesCount + 1 ) . " files to test." );
+    echo '> ' . \ZincPHP\CLI\Helper\success( $tester->blocksCount . " blocks and " . $tester->testFilesCount . " files to test." );
     \ZincPHP\CLI\Helper\nl();
     echo $tester->run();
     // print_r( $testables );
