888c8eedfd0a81ed613deaee93e516511c65cdcb
Kazi Mehedi Hasan <rakibtg@gmail.com>
2018-02-18 14:55:33 +0600

    Dynamically detect the method of request

--- app/core/zincphp_tester/BlockTester.php
+++ app/core/zincphp_tester/BlockTester.php
@@ -1,19 +1,27 @@
 <?php
 
+  require_once __DIR__ . '/TestsTraits.php';
+  
   class BlockTester {
+
+    use TestsTraits;
 
     public $requestUrl;
     public $parameters;
+    public $fetchedResponse;
     public $expectedResponseStatus;
     public $expectEmptyResponse;
     public $expectedData;
     public $responseDataValidator;
+    public $expectedContentType;
+    public $requestMethod;
 
-    // private function makeRequest() {
+    private function makeRequest( $requester ) {
+      $_funcName = "HTTP" . ucfirst( $this->requestMethod );
+      $this->fetchedResponse = $requester->$_funcName( $this->requestUrl, $this->parameters );
+    }
 
-    // }
-
-    public function generateUrlFromPath( $requestUrl ) {
+    public function generateUrlFromPath( $requestUrl, $devServer ) {
       $requestUrl = preg_replace( '/\/tests$/', '', $requestUrl );
       $pos = strpos( $requestUrl, 'blocks' );
       if ( $pos !== false ) {
@@ -21,12 +29,17 @@
       }
       if ( isset( explode( '456789238473892___block', $requestUrl )[ 1 ] ) ) {
         $this->requestUrl = explode( '456789238473892___block', $requestUrl )[ 1 ];
-        print "Request URL: " . $this->requestUrl . "\n\n";
       }
+
     }
 
-    public function runTest() {
+    public function setRequestMethod( $testFileName ) {
+      $_method = explode( '.', $testFileName );
+      $this->requestMethod = $_method[ 0 ];
+    }
 
+    public function runTest( $requester ) {
+      $this->makeRequest( $requester );
       // print_r( $this->parameters );
       // echo "\n";
       // print_r( $this->expectedResponseStatus );
