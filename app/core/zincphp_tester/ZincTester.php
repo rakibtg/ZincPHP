24424e8f836c7b56445c5517f48d24d4d0ef8f73
Kazi Mehedi Hasan <rakibtg@gmail.com>
2018-02-18 14:56:26 +0600

    Added dev URL and working on tes run() method

--- app/core/zincphp_tester/ZincTester.php
+++ app/core/zincphp_tester/ZincTester.php
@@ -1,14 +1,23 @@
 <?php
 
+  require_once __DIR__ . '/../ZincHTTP.php';
+
   class ZincTester {
+
     public $blocksCount;
     public $testFilesCount;
     public $testables;
+    public $devServer;
 
     function __construct() {
+
       $this->blocksCount    = 0;
       $this->testFilesCount = 0;
       $this->testables      = [];
+      $this->devServer      = '127.0.0.1:8585';
+
+      $this->getTestableBlocks();
+
     }
 
     function makeTestClassName( $blockName ) {
@@ -19,14 +28,14 @@
       return ucfirst( $blockName );
     }
 
-    function getTestDirectories( $dir = false, &$results = [] ) {
+    function getTestableBlocks( $dir = false, &$results = [] ) {
       if ( $dir === false ) $dir = './blocks';
       $files = scandir( $dir );
       foreach( $files as $key => $value ){
         $path = realpath( $dir . DIRECTORY_SEPARATOR . $value );
         if( is_dir( $path ) ) {
           if( $value != "." && $value != ".." ) {
-            $this->getTestDirectories( $path, $results );
+            $this->getTestableBlocks( $path, $results );
             $_testDir = explode( DIRECTORY_SEPARATOR, $path );
             if ( $_testDir[ count( $_testDir ) - 1 ] === 'tests' ) {
               $testable[ 'block' ] = $_testDir[ count( $_testDir ) - 2 ];
@@ -51,8 +60,35 @@
       return $results;
     }
 
-    public function run() {
+    public function run( $argv ) {
+
+      // Set the dev domain.
+      if ( is_array( $argv ) ) {
+        foreach ( $argv as $arg ) {
+          if ( strpos( $arg, '=' ) !== false ) {
+            $_ds = explode( '=', $arg );
+            if ( trim( $_ds[ 0 ] ) === '--server' ) {
+              if ( isset( $_ds[ 1 ] ) ) {
+                $this->devServer = $_ds[ 1 ];
+              }
+            }
+          }
+        }
+      }
+
+      \ZincPHP\CLI\Helper\nl();
+      echo 'Starting test...';
+      \ZincPHP\CLI\Helper\nl();
+
       if ( count( $this->testables ) > 0 ) {
+
+        echo \ZincPHP\CLI\Helper\success( $this->blocksCount . " blocks and " . $this->testFilesCount . " files to test." );
+        \ZincPHP\CLI\Helper\nl();
+        \ZincPHP\CLI\Helper\nl();
+
+        // Create new instance of the zinc http module.
+        $requester = new ZincHTTP();
+
         // We have some testables blocks.
         foreach ( $this->testables as $testBlock ) {
           // One block might have more than one test file.
@@ -60,13 +96,18 @@
             // Importing the test file.
             require_once $testBlock[ 'path' ] . DIRECTORY_SEPARATOR . $testFile;
             // Start the test.
-            $_className = $this->makeTestClassName( $testFile ); // Generate the class name of the test class.
+            $_className  = $this->makeTestClassName( $testFile ); // Generate the class name of the test class.
             $blockTester = new $_className(); // Dynamically create a new instance of the test class.
-            $blockTester->generateUrlFromPath( $testBlock[ 'path' ] ); // Passing the block name into the block tester class.
+            $blockTester->generateUrlFromPath( $testBlock[ 'path' ], $this->devServer ); // Passing the block name into the block tester class.
+            $blockTester->setRequestMethod( $testFile );
             $blockTester->makeTest();
-            $blockTester->runTest();
+            $blockTester->runTest( $requester );
           }
-        }
+        } // End of $this->testables loop.
+
+      } else {
+        echo \ZincPHP\CLI\Helper\danger( "No tests found!" );
+        \ZincPHP\CLI\Helper\nl();
       }
     }
 
