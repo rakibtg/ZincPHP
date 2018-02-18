7f5d61999f4aeb751452c9fc70d96823c578f77d
Kazi Mehedi Hasan <rakibtg@gmail.com>
2018-02-18 14:54:24 +0600

    Added header informations with HTTP Requests

--- app/core/ZincHTTP.php
+++ app/core/ZincHTTP.php
@@ -4,6 +4,50 @@
  * Send HTTP request to an REST end-point.
  *
  */
+
+
+/*
+
+  A SAMPLE RESPONSE DATA
+  [
+    {
+      "content": "hello world",
+      "header": {
+        "url": "http:\/\/127.0.0.1:2080\/?route=test-make-request",
+        "content_type": "application\/json; charset=utf-8",
+        "http_code": 200,
+        "header_size": 202,
+        "request_size": 146,
+        "filetime": -1,
+        "ssl_verify_result": 0,
+        "redirect_count": 0,
+        "total_time": 0.001458,
+        "namelookup_time": 6.0e-5,
+        "connect_time": 0.00032,
+        "pretransfer_time": 0.000441,
+        "size_upload": 0,
+        "size_download": 25,
+        "speed_download": 17146,
+        "speed_upload": 0,
+        "download_content_length": -1,
+        "upload_content_length": 0,
+        "starttransfer_time": 0.001327,
+        "redirect_time": 0,
+        "redirect_url": "",
+        "primary_ip": "127.0.0.1",
+        "certinfo": [],
+        "primary_port": 2080,
+        "local_ip": "127.0.0.1",
+        "local_port": 48964
+      }
+    },
+    ...
+  ]
+
+*/
+
+
+
 class ZincHTTP {
   /**
    * Make HTTP-GET call
@@ -16,7 +60,8 @@ class ZincHTTP {
     $ch    = curl_init( $url . '?' . $query );
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
     curl_setopt( $ch, CURLOPT_HEADER, false );
-    $response = curl_exec( $ch );
+    $response[ 'content' ] = curl_exec( $ch );
+    $response[ 'header' ] = curl_getinfo( $ch );
     curl_close( $ch );
     return $response;
   }
@@ -35,7 +80,8 @@ class ZincHTTP {
     curl_setopt( $ch, CURLOPT_URL, $url );
     curl_setopt( $ch, CURLOPT_POST, true );
     curl_setopt( $ch, CURLOPT_POSTFIELDS, $query );
-    $response = curl_exec( $ch );
+    $response[ 'content' ] = curl_exec( $ch );
+    $response[ 'header' ] = curl_getinfo( $ch );
     curl_close( $ch );
     return $response;
   }
@@ -55,7 +101,8 @@ class ZincHTTP {
     \curl_setopt( $ch, \CURLOPT_URL, $url );
     \curl_setopt( $ch, \CURLOPT_CUSTOMREQUEST, $method );
     \curl_setopt( $ch, \CURLOPT_POSTFIELDS, $query );
-    $response = \curl_exec( $ch );
+    $response[ 'content' ] = curl_exec( $ch );
+    $response[ 'header' ] = curl_getinfo( $ch );
     \curl_close( $ch );
     return $response;
   }
