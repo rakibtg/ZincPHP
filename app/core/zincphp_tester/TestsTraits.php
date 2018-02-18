1f160ecc4beb497aafdc3b2f8d10c0e2dfdab2a3
Kazi Mehedi Hasan <rakibtg@gmail.com>
2018-02-18 14:55:52 +0600

    Working on test trait functions

---
+++ app/core/zincphp_tester/TestsTraits.php
@@ -0,0 +1,35 @@
+<?php
+
+  /**
+    Expected Response Status
+    Expected Content Type
+    Expect Empty Response
+    Expected Data
+    Response Data Validator
+   */
+
+  trait TestsTraits {
+
+    public function testStatus() {
+
+    }
+
+    public function testContentType() {
+
+    }
+
+    public function testEmptyResponse() {
+
+    }
+
+    public function testExactResponseData() {
+
+    }
+
+    public function dataValidator() {
+
+    }
+
+    
+
+  }
