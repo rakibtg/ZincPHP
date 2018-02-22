b389cb34565c3894f323ce9791f2809dc7e3d3fa
rakibtg <rakibtg@gmail.com>
2018-02-22 08:31:50 +0600

    Changes for test file

--- blocks/tests/testing/unit-test/tests/put.unit-test.test.php
+++ blocks/tests/testing/unit-test/tests/put.unit-test.test.php
@@ -7,18 +7,29 @@
       $this->setHeaders( [] );
       $this->setParameters( [] );
     }
-
+ 
     public function setExpectations () {
-      $this->expectedResponseStatus = 200;
-      $this->expectedContentType = 'application/json';
-      $this->expectEmptyResponse = false;
-      $this->expectedData = ['name' => 'Hello World!'];
-      $this->responseDataValidator = [
+      // $this->expectedResponseStatus = 200;
+      $this->expectedHTTPCode( 200 );
+      // $this->expectedContentTypeValue = 'application/json';
+      $this->expectedContentType( 'application/json' );
+      // $this->expectEmptyResponse = false;
+      // $this->expectedDataValue = ['name' => 'Hello World!'];
+      $this->expectedData( [ 'name' => 'Hello World!' ] );
+      // $this->responseDataValidator = [
+      //   'name' => [
+      //     'rules' => 'required|lengthMax:120',
+      //     'value' => $this->getResponseData( 'name' ) // $this->getResponseData()[ 0 ][ 'name ] ; just check the first item of a array of result.
+      //   ]
+      // ];
+      $rules = [
         'name' => [
-          'rules' => 'required|lengthMax:120',
+          'rules' => 'required|email|array|max:2',
           'value' => $this->getResponseData( 'name' ) // $this->getResponseData()[ 0 ][ 'name ] ; just check the first item of a array of result.
         ]
       ];
+      $this->validateExpectedData( $rules );
+      
     }
 
   }
