af71d04e70249a14d53d1c7548834931f516a313
rakibtg <rakibtg@gmail.com>
2018-02-25 08:26:15 +0600

    testing tests

--- blocks/tests/testing/unit-test/tests/put.unit-test.test.php
+++ blocks/tests/testing/unit-test/tests/put.unit-test.test.php
@@ -4,32 +4,24 @@
   class Putunittest extends BlockTester {
 
     public function metaData () {
+      print __DIR__ . "\n";
       $this->setHeaders( [] );
       $this->setParameters( [] );
+      $this->setFiles( [
+        'photo' => '/girl.jpg'
+      ] );
     }
- 
+
     public function setExpectations () {
-      // $this->expectedResponseStatus = 200;
       $this->expectedHTTPCode( 200 );
-      // $this->expectedContentTypeValue = 'application/json';
       $this->expectedContentType( 'application/json' );
-      // $this->expectEmptyResponse = false;
-      // $this->expectedDataValue = ['name' => 'Hello World!'];
       $this->expectedData( [ 'name' => 'Hello World!' ] );
-      // $this->responseDataValidator = [
-      //   'name' => [
-      //     'rules' => 'required|lengthMax:120',
-      //     'value' => $this->getResponseData( 'name' ) // $this->getResponseData()[ 0 ][ 'name ] ; just check the first item of a array of result.
-      //   ]
-      // ];
-      $rules = [
+      $this->validateExpectedData( [
         'name' => [
-          'rules' => 'required|email|array|max:2',
-          'value' => $this->getResponseData( 'name' ) // $this->getResponseData()[ 0 ][ 'name ] ; just check the first item of a array of result.
+          'rules' => 'required',
+          'value' => $this->getResponseData( 'name' )
         ]
-      ];
-      $this->validateExpectedData( $rules );
-      
+      ] );
     }
 
   }
