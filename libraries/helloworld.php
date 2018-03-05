8bf945595e8cd39a93ccb35c6f6a1c03bb5a89cf
rakibtg <rakibtg@gmail.com>
2018-03-06 01:50:44 +0600

    Strted working on user library

---
+++ libraries/helloworld.php
@@ -0,0 +1,19 @@
+<?php
+
+  /*
+
+    This is a test library, that only has a 
+    simple function.
+  
+  */
+
+  namespace HelloWorld;
+
+  /**
+   * Say hello to the world!
+   * 
+   * @return string
+   */
+  function helloworld() {
+    return 'Hello World!';
+  }
