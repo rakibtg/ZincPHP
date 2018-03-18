cbf465042e0295ef5e5bfdf19120a3395da60169
rakibtg <rakibtg@gmail.com>
2018-03-19 00:17:09 +0600

    Implemented log method for the console

--- app/core/zincphp_consoler/Console.php
+++ app/core/zincphp_consoler/Console.php
@@ -2,11 +2,26 @@
 
   class Console {
 
-    public function log(  ) {
-
+    public function log ( $data = '' ) {
+      // Loggable data.
+      $logs = [
+        'logged_at'   => date( 'g:i:s A', time() ),
+        'logged_data' => '',
+        'data_type'   => gettype( $data ),
+      ];
+      // Process loggable data.
+      if ( $logs[ 'data_type' ] === 'boolean' ) {
+        $logs[ 'logged_data' ] = $data === true ? 'true' : 'false';
+      } else if ( $logs[ 'data_type' ] === 'array' OR $logs[ 'data_type' ] === 'object' ) {
+        $logs[ 'logged_data' ] = $data;
+      } else {
+        $logs[ 'logged_data' ] = ( string ) $data;
+      }
+      // Make temporary log file.
+      file_put_contents( __DIR__ . '/temporary_logs/' . time() . mt_rand( 99, 99999 ) . '.log', json_encode( $logs ) );
     }
 
-    public function reader() {
+    public function reader () {
       $data = '';
       $_type = gettype( $data );
       if ( $_type === 'boolean' ) {
