c2a6e54003be107b9976e1ae66b3f1d5f1a27e80
rakibtg <rakibtg@gmail.com>
2018-05-09 21:50:20 +0600

    Fixed the random_bytes issue and added support for php5

--- app/core/app_traits/HelperTraits.php
+++ app/core/app_traits/HelperTraits.php
@@ -83,21 +83,13 @@
      */
     public static function randomString( $length = 10 ) {
       if ( function_exists( 'random_bytes' ) ) {
-        return random_bytes( $length );
+        return bin2hex( random_bytes( $length ) );
       } else {
         $keyspace = '0123456789abcdefg.hijklmnopqrstuvwxyzA-BCDEFGHIJK_LMNOPQRSTU#VWXYZ';
         $str = '';
-        if( function_exists( 'mb_strlen' ) ) {
-          $max = mb_strlen( $keyspace, '8bit' ) - 1;
-        } else {
-          $max = strlen( $keyspace );
-        }
+        $max = strlen( $keyspace );
         for ( $i = 0; $i < $length; ++$i ) {
-          if ( function_exists( 'random_int' ) ) {
-            $str .= $keyspace[ random_int( 0, $max ) ];
-          } else {
-            $str .= $keyspace[ mt_rand( 0, $max ) ];
-          }
+          $str .= $keyspace[ mt_rand( 0, $max ) ];
         }
         return $str;
       }
