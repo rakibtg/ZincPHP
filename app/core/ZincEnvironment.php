1a4624863319f70e7fffcd70039f1825a1b8ad5e
rakibtg <rakibtg@gmail.com>
2018-05-22 00:34:05 +0600

    Environment class to manage the environment.json document

---
+++ app/core/ZincEnvironment.php
@@ -0,0 +1,54 @@
+<?php
+
+  /**
+   * Manage the environment file of the ZincPHP framework.
+   * 
+   */
+
+  class ZincEnvironment {
+
+    /**
+     * Cache the instance.
+     * 
+     * @var object $instance
+     */
+    private static $instance = null;
+
+    /**
+     * Cache the environment settings.
+     * 
+     * @var object $env
+     */
+    private $env = null;
+
+    /**
+     * Create a new instance of this class if not already.
+     * 
+     * @return object
+     */
+    public static function getInstance() {
+      if ( ! self::$instance ) self::$instance = new ZincEnvironment();
+      return self::$instance;
+    }
+
+    /**
+     * Read the environment file if exists.
+     * 
+     * @return object
+     */
+    public function readEnvFile() {
+      if ( ! $this->env ) {
+        $envPath = __DIR__ . '/../environment.json';
+        // Import and set environment variables from environment document.
+        if( ! file_exists( $envPath ) ) {
+          App::responseError( 'Environment document was not found! 
+            Run \'php zinc env:new\' command to create a new environment document.' );
+          exit();
+        }
+        // Set environment settings
+        $this->env = json_decode( file_get_contents( $envPath ) );
+      }
+      return $this->env;
+    }
+
+  }
