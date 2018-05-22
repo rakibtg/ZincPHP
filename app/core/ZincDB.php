095b60fe9eeee606d6ba0739ea80e8de327df6f5
rakibtg <rakibtg@gmail.com>
2018-05-22 22:07:18 +0600

    Class that connects pixie with ZincPHP to query the database

---
+++ app/core/ZincDB.php
@@ -0,0 +1,39 @@
+<?php 
+
+/**
+ * ZincDB is a helper core class of ZincPHP framework that basically
+ * makes a connection between the pixie package.
+ * 
+ */
+
+class ZincDB {
+
+  /**
+   * Cache the instance.
+   * 
+   * @var object $instance
+   */
+  private static $instance = null;
+
+  /**
+   * Cache the query builder instance.
+   * 
+   * @var object $instance
+   */
+  private $queryBuilder = null;
+
+  public static function getInstance() {
+    if ( ! self::$instance ) self::$instance = new ZincDB();
+    return self::$instance;
+  }
+
+  public function newQB() {
+    if ( ! $this->queryBuilder ) {
+      $zpEnv = (array) App::environment()->database_config;
+      $connection = new \Pixie\Connection( $zpEnv[ 'driver' ], $zpEnv );
+      $this->queryBuilder = new \Pixie\QueryBuilder\QueryBuilderHandler( $connection );
+    }
+    return $this->queryBuilder;
+  }
+
+}
