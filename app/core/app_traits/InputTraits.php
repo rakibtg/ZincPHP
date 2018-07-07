dd2d452c245811238a6e98b216714c50d0ba3875
rakibtg <rakibtg@gmail.com>
2018-07-08 01:48:19 +0600

    Allow PATCH requests

--- app/core/app_traits/InputTraits.php
+++ app/core/app_traits/InputTraits.php
@@ -14,8 +14,40 @@
      * @return  string Returns null if the key is not found by default.
      */
     public static function input ( $fieldName = false ) {
+
       $method = App::requestType();
-      return App::$method( $fieldName );
+
+      // Detecting the content type of the request.
+      if ( ! isset( $_SERVER[ "CONTENT_TYPE" ] ) ) {
+
+        // No content type was found. Setting up default.
+        $contentType = 'text/plain';
+
+      } else {
+
+        $contentType = explode( ';', $_SERVER[ "CONTENT_TYPE" ] );
+        if ( isset( $contentType[ 0 ] ) ) {
+
+          if ( ! empty( $contentType[ 0 ] ) ) $contentType = trim( $contentType[ 0 ] );
+          else $contentType = 'text/plain';
+
+        } else $contentType = 'text/plain';
+
+      }
+
+      // We dont need a check if GET method
+      if ( $method === 'get' ) return $_GET;
+
+      // Encode data based on content type
+      if ( $contentType === 'application/x-www-form-urlencoded' ) {
+        if ( $method === 'post' ) return $_POST;
+        else return json_decode( file_get_contents( "php://input" ), true );
+      } else {
+        return json_decode( file_get_contents( "php://input" ), true );
+      }
+
+      return [];
+
     }
 
     /**
