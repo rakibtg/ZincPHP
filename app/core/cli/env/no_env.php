2d9d5f35b8aced7816209a5a30e0723b8e3896bd
Kazi Mehedi Hasan <rakibtg@gmail.com>
2018-07-10 14:32:53 +0600

    Namespace aliasing no  environment

--- app/core/cli/env/no_env.php
+++ app/core/cli/env/no_env.php
@@ -1,7 +1,17 @@
 <?php
 
-echo \ZincPHP\CLI\Helper\danger( "ERROR" ) . "No environment document was found!";
-\ZincPHP\CLI\Helper\nl();
-echo \ZincPHP\CLI\Helper\warn( "TIPS " )."Run this command ".\ZincPHP\CLI\Helper\success( 'php zinc env:new' )."to generate a new env file.";
-\ZincPHP\CLI\Helper\nl();
+use \ZincPHP\CLI\Helper as CLI;
+
+echo CLI\danger( "ERROR" ) . "No environment document was found!";
+
+CLI\nl();
+
+echo CLI\warn( "TIPS " ) 
+  . "Run this command "
+  . CLI\success( 'php zinc env:new' )
+  . "to generate a new env file.";
+
+CLI\nl();
+
+// Exit from the CLI
 exit();
