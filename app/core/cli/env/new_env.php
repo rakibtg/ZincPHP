330f6717765e88a9c5e68630f7fb92268e96d4dc
Kazi Mehedi Hasan <rakibtg@gmail.com>
2018-07-10 14:32:42 +0600

    Namespace aliasing new environment

--- app/core/cli/env/new_env.php
+++ app/core/cli/env/new_env.php
@@ -1,25 +1,37 @@
 <?php
 
+  use \ZincPHP\CLI\Helper as CLI;
   require_once './app/core/app_getter/App.php';
 
   // Check if a env file already exists.
   if( file_exists( './app/environment.json' ) ) {
     echo "environment.json already exists, do you want to overwrite?(Y/n) ";
+
     $handle = fopen( "php://stdin", "r" );
     $cont   = trim( fgets( $handle ) );
+
     if( strtolower( $cont ) == 'y' ) {
       echo "Overwriting environment.json file ...";
-      \ZincPHP\CLI\Helper\nl();
+      CLI\nl();
     } else {
       echo "Action canceled";
-      \ZincPHP\CLI\Helper\nl();
+      CLI\nl();
+      // Exit CLI
       exit();
     }
+
   }
+
   // Create the new environment.json file.
-  $default_env = json_decode( file_get_contents( './app/core/cli/zincphp_structures/environment.json.example' ) );
-  $default_env->secret_keys = bin2hex( App::randomString( 64 ) );
+  $default_env = json_decode( 
+    file_get_contents( './app/core/cli/zincphp_structures/environment.json.example' ) 
+  );
+
+  $default_env->secret_keys = App::randomString( 100 );
   file_put_contents( './app/environment.json', json_encode( $default_env, JSON_PRETTY_PRINT ) );
-  echo \ZincPHP\CLI\Helper\success( "Environment document has created." );
-  \ZincPHP\CLI\Helper\nl();
+  echo CLI\success( "Environment document has created." );
+
+  CLI\nl();
+
+  // Exit CLI
   exit();
