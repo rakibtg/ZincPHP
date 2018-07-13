0e3c822b0dc9f4f85237b2bf9f95e7aed2419c88
rakibtg <rakibtg@gmail.com>
2018-07-13 22:18:49 +0600

    Added PATCH as allowed request

--- app/core/zinc/Zinc.php
+++ app/core/zinc/Zinc.php
@@ -38,7 +38,7 @@
           foreach( $env->cors_allowed as $_dmn ) {
             if( $http_origin == $_dmn ) {
               header( 'Access-Control-Allow-Origin: ' . $http_origin );
-              header( 'Access-Control-Allow-Methods: ' . 'GET, POST, PUT, DELETE, PATCH' );
+              header( 'Access-Control-Allow-Methods: ' . 'GET, POST, PUT, DELETE, PATCH, OPTIONS' );
               header( 'Access-Control-Allow-Headers: Origin, Content-Type, Authorization' );
               if ( $requestMethod == 'options' ) {
                 // This request is a CORS preflight.
