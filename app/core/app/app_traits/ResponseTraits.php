05fe0332a15dc0a967e23fefd5d7f2474ec9a8b1
rakibtg <rakibtg@gmail.com>
2018-09-20 00:42:07 +0600

    Typo fixes

---
+++ app/core/app/app_traits/ResponseTraits.php
@@ -0,0 +1,51 @@
+<?php
+
+  trait ResponseTraits {
+
+    /**
+     * Converts data to json then print it.
+     *
+     * @param array|object|string   $data           Data to output
+     * @param integer               $responseStatus Set the response status code
+     * @param boolean               $prettyPrint    Pretty prints the output JSON
+     * @return void
+     */
+    public static function response( $data = [], $responseStatus = 200, $prettyPrint = false, $contentType = 'application/json' ) {
+
+      // Setting the response code of the output.
+      http_response_code( $responseStatus );
+
+      // Setting the response content type.
+      header( 'Content-Type: ' . $contentType . '; charset=utf-8' );
+
+      if( empty( $data ) ) {
+        echo json_encode( [] );
+      } else if ( is_array( $data ) ) {
+        if( $prettyPrint ) echo json_encode( $data, JSON_PRETTY_PRINT );
+        else echo json_encode( $data );
+      } else {
+        echo json_encode( $data );
+      }
+      exit();
+    }
+
+    /**
+     * Returns error with custom status response.
+     * If a error message was not provided then the default
+     * error message will be printed.
+     *
+     * @param   string    $msg        Custom error message
+     * @param   integer   $status     HTTP status code, default is 404 (Not found)
+     * @return  void
+     */
+    public static function responseError( $msg = false, $status = 404 ) {
+      http_response_code( $status );
+      if( ! $msg ) $msg = 'Unexpected error occurred';
+      $msg = (array) $msg;
+      echo json_encode( [
+        'message' => $msg,
+      ] );
+      exit();
+    }
+
+  }
