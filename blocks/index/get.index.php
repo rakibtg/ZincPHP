a4a29391c5351140c38ad9ade46f72a16509b1c2
rakibtg <rakibtg@gmail.com>
2018-09-21 02:35:54 +0600

    Changes according to the new response method

--- blocks/index/get.index.php
+++ blocks/index/get.index.php
@@ -1,16 +1,14 @@
 <?php
 
   /**
-   * A sample welcome block.
-   * Learn more about blocks in the documentation.
-   * @link http://...
+   * A sample greeting block.
+   * 
+   * @link https://getzincphp.github.io/docs/latest/Blocks
    */
 
-  // Welcome message data.
-  $welcomeMessage = [
-    'ZincPHP'   => 'Welcome to ZincPHP',
-    'ToDo'      => 'To edit this response, go to \'get.index.php\' inside \'blocks\' directory',
-    'you'       => 'Hello ' . \App::input( 'me' ) . '!'
+  $greetings = [
+    'greetings' => 'Welcome to ZincPHP',
+    'to_do' => 'To edit this response, go to \'get.index.php\' inside \'blocks\' directory'
   ];
 
-  App::response( $data = $welcomeMessage, $responseStatus = 200, $prettyPrint = true );
+  App::response()->data( $greetings )->pretty()->send();
