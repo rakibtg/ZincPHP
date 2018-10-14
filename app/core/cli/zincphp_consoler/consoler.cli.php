51db306df9c61b75b83993c23f3aa1959a636232
rakibtg <rakibtg@gmail.com>
2018-10-14 23:03:22 +0600

    code cleanup and consoler namespace

--- app/core/cli/zincphp_consoler/consoler.cli.php
+++ app/core/cli/zincphp_consoler/consoler.cli.php
@@ -4,19 +4,17 @@
   set_time_limit(0);
 
   require_once __DIR__ . '/Console.php';
-  $consoler = new Console();
+  $consoler = new \ZincPHP\Console\Console();
 
   echo CLI\success( "ZincPHP Consoler is looking for logs" );
   echo CLI\nl();
   echo CLI\warn( "Press Ctrl + C to stop consoler." );
   echo CLI\nl();
-  echo '_______________________________________';
-  echo CLI\nl();
   echo CLI\nl();
 
   while( true ) {
     $consoler->reader();
-    sleep( 1 );
+    sleep(2);
   }
 
   exit();
