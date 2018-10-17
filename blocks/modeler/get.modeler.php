95c663d628b92a75d418bd460ef3f4486610c8e8
rakibtg <rakibtg@gmail.com>
2018-10-17 23:32:50 +0600

    implemented the new model method for testing

--- blocks/modeler/get.modeler.php
+++ blocks/modeler/get.modeler.php
@@ -6,15 +6,31 @@
    * @link https://getzincphp.github.io/docs/latest/Blocks
    */
 
-  $x = \App::db(false, true);
-  $x->bootEloquent();
-
   $users = \App::model( 'v1/User' );
+
   $greetings = [
     'greetings' => 'Working on models to work in ZincPHP! 😍',
     'model' => $users->paginate(20)
   ];
+  /*
+    $i1 = \App::model( 'v1/User' );
+    $i1->name = uniqid();
+    $i1->email = uniqid() . '@gmail.com';
+    $i1->bio = uniqid();
+    $i1->save();
 
+    $i2 = \App::model( 'v1/User' );
+    $i2->name = uniqid();
+    $i2->email = uniqid() . '@gmail.com';
+    $i2->bio = uniqid();
+    $i2->save();
+
+    $i3 = \App::model( 'v1/User' );
+    $i3->name = uniqid();
+    $i3->email = uniqid() . '@gmail.com';
+    $i3->bio = uniqid();
+    $i3->save();
+  */
   \App::response()
     ->data( $greetings )
     ->pretty()
