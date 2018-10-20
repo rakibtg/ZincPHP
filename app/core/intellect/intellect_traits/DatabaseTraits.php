3d5b162b0951185fea784961b27434df90479310
rakibtg <rakibtg@gmail.com>
2018-10-20 09:20:31 +0600

    Instantiate models with proper namespace

--- app/core/intellect/intellect_traits/DatabaseTraits.php
+++ app/core/intellect/intellect_traits/DatabaseTraits.php
@@ -6,32 +6,32 @@
      * 
      */
     public static function model( $modelPath = '' ) {
-      if(empty($modelPath)) throw new Exception("Model name can't be empty");
+      if(empty( $modelPath )) throw new Exception( "Model name can't be empty" );
 
-      $modelPath = trim((string) $modelPath);
+      $modelPath = trim(( string ) $modelPath );
+      if( $modelPath[ 0 ] !== '/' ) $modelPath = '/' . $modelPath;
+
+      // Generate the namespace of the model.
+      $modelNamespace = 'ZincPHP\models' . str_replace( '/', '\\', $modelPath ) . '\\';
 
       // Generate the absolute path of the model file.
-      if($modelPath[0] !== "/") $modelPath = self::dir("models") . '/' . $modelPath;
-      else $modelPath = self::dir("models") . $modelPath;
-
-      $modelClassName = basename($modelPath);
+      $modelPath = self::dir( 'models' ) . $modelPath;
+      $modelClassName = basename( $modelPath );
       $modelPath = $modelPath . ".php";
 
       // Check if the model file exists or not.
-      if(!file_exists($modelPath)) throw new Exception("Model not found at " . $modelPath);
+      if(!file_exists( $modelPath )) throw new Exception( "Model not found at " . $modelPath );
 
       // Boot model connections.
       self::bootModel();
 
       // Import the model file.
-      require_once $modelPath;
-      return new $modelClassName();
-
-      return [
-        "path" => $modelPath,
-        "file" => $modelClassName
-      ];
+      $_class = $modelNamespace . $modelClassName;
+      try {
+        require_once $modelPath;
+        return new $_class();
+      } catch( Exception $e ) {
+        self::exception( $e );
+      }
     }
-
-
   }
