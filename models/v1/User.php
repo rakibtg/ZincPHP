44c30ce67f1811b74fd8dbbf495159e2c5201b04
rakibtg <rakibtg@gmail.com>
2018-10-20 09:20:43 +0600

    testable model files

--- models/v1/User.php
+++ models/v1/User.php
@@ -1,6 +1,7 @@
 <?php
+  namespace ZincPHP\models\v1\User;
 
-  class User extends ZincModel {
+  class User extends \ZincPHP\model\ZincModel {
     public function posts() {
       return $this->hasMany( \App::model('v1/Post'), 'author' );
     }
