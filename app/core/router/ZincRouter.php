9763f2abbe12b102958f2293614072dccaf675f6
rakibtg <rakibtg@gmail.com>
2019-02-17 20:01:39 +0600

    implementing middleware and class based blocks

--- app/core/router/ZincRouter.php
+++ app/core/router/ZincRouter.php
@@ -1,6 +1,8 @@
 <?php
 
   namespace ZincPHP\Route;
+  require_once __DIR__ . '/HandleBlock.php';
+  require_once __DIR__ . '/HandleMiddleware.php';
 
   /**
    * Route HTTP requests to proper block.
@@ -9,12 +11,14 @@
 
   class ZincRouter {
 
-  /**
-   * The entire app object, that contains every ZincPHP methods.
-   * Zinc router(this class) will not be a part of this app object.
-   * 
-   * @var object $app
-   */
+    use HandleBlockTraits, HandleMiddlewareTraits;
+
+    /**
+     * The entire app object, that contains every ZincPHP methods.
+     * Zinc router(this class) will not be a part of this app object.
+     * 
+     * @var object $app
+     */
     protected $app;
 
     /**
@@ -24,6 +28,27 @@
     public $blockName;
 
     /**
+     * The method type of the request.
+     * @var string
+     */
+    public $requestType;
+
+    /**
+     * Keeps a safe version of the url segments.
+     * @var string
+     */
+    protected $segments;
+
+    /**
+     * Safe block name without any extension or method type.
+     * @var string
+     */
+    protected $blockLabel;
+
+    private $blockHandler;
+    private $middlewareHandler;
+
+    /**
      * Current route query string data.
      * @var string
      */
@@ -31,7 +56,10 @@
 
     function __construct() {
       // Get the current route path.
-      $this->route = \App::strTrim( $this->fetchUrl() );
+      $this->route = \App::string( $this->makeSegments() )
+        ->trim();
+      $this->requestType = \App::requestType();
+      $this->blockHandler = new HandleBlock();
     }
 
     /**
@@ -40,28 +68,45 @@
      * 
      */
     public function boot( $zincObject ) {
-
       /**
        * Initializing the Zinc object to the app protected variable.
        * So, every features are accessible from the route.
        */
       $this->app = $zincObject;
-
       if( empty( $this->route ) ) {
         // No block was given.
-        $this->goToDefaultBlock();
-      } else {
-        // Block was detected in the query string.
-        $this->goToCurrentBlock();
+        $this->route = 'index';
+        // $this->goToDefaultBlock();
+      } 
+      // Prepare segments.
+      $segments = '/';
+      // For security purposes recasting the url.
+      foreach( explode( '/', $this->route ) as $uri ) {
+        $segments = $segments . $uri . '/';
       }
+      // Remove extra // from the segments.
+      $this->segments = \App::string($segments)
+        ->trim('/')
+        ->prepend('/');
+      // Get the block label.
+      $this->blockLabel = basename( $this->segments );
+      // Prepare the full absolute block path.
+      $this->blockAbsolutePath = \App::dir('blocks') 
+        . $this->segments 
+        . '/' 
+        . $this->requestType
+        . '.'
+        . $this->blockLabel
+        . '.php';
+      $this->goToCurrentBlock();
     }
 
     /**
-     * Fetch destination from the URL.
+     * Extract segments of the URL.
      * 
      * @return  string  URI Segments
      */
-    public function fetchUrl() {
+    public function makeSegments() {
       // If we dont use Apache/Ngnix then we will get the URI from PATH_INFO
       if( isset( $_SERVER[ 'PATH_INFO' ] ) ) {
         if( ! empty( $_SERVER[ 'PATH_INFO' ] ) ) return $_SERVER[ 'PATH_INFO' ];
@@ -83,74 +128,56 @@
       return ''; // Finally return empty string, if no URI segments was found.
     }
 
-    public function initializeMiddleware() {
-      $definations = \App::dir('middlewares') 
-        . '/' 
-        . 'init.middleware.php';
-      if(file_exists($definations)) {
-        require_once $definations;
-        if(isset($init)) { // $init variable lives in the init.middleware.php file.
-          return $init;
-        }
-      }
-      return false;
-    }
+    // public function initializeMiddleware() {
+    //   $definations = \App::dir('middlewares') 
+    //     . '/' 
+    //     . 'init.middleware.php';
+    //   if(file_exists($definations)) {
+    //     require_once $definations;
+    //     if(isset($init)) { // $init variable lives in the init.middleware.php file.
+    //       return $init;
+    //     }
+    //   }
+    //   return false;
+    // }
 
-    public function handleMiddleware($segments) {
-      $init = $this->initializeMiddleware();
-      if( $init !== false ) {
-        $currentBlock = trim(\App::string($segments)->trim('/'));
-        if(isset($init[$currentBlock])) {
-          foreach($init[$currentBlock] as $middlewareFile) {
-            // Include the middleware function.
-            require_once \App::dir('middlewares') 
-              . '/' 
-              . $middlewareFile 
-              . '.php';
-            // Call the middleware function.
-            ($middlewareFile)();
-          }
-        }
-      }
-    }
+    // public function handleMiddleware($segments) {
+    //   $init = $this->initializeMiddleware();
+    //   if( $init !== false ) {
+    //     $currentBlock = trim(\App::string($segments)->trim('/'));
+    //     if(isset($init[$currentBlock])) {
+    //       foreach($init[$currentBlock] as $middlewareFile) {
+    //         // Include the middleware function.
+    //         require_once \App::dir('middlewares') 
+    //           . '/' 
+    //           . $middlewareFile 
+    //           . '.php';
+    //         // Call the middleware function.
+    //         ($middlewareFile)();
+    //       }
+    //     }
+    //   }
+    // }
 
     /**
      * Go to the block based on its request type.
      *
+     * @param     none
+     * @return    none
      */
     public function goToCurrentBlock() {
 
-      // Request method
-      $requestMethod = \App::requestType();
-
-      // Check if block exist.
-      $segments = '/';
-      // For security purposes recasting the url splitted by segments.
-      foreach( explode( '/', $this->route ) as $uri ) {
-        $segments = $segments . $uri . '/';
-      }
-
-      // Remove extra // from the segments.
-      $segments = \App::string($segments)
-        ->trim('/')
-        ->prepend('/');
-
-      // Add the request type with the block name (with the basename of the block path)
-      $this->blockName = 
-        \App::dir('blocks') 
-          . $segments 
-          . '/' 
-          . $requestMethod
-          . '.'
-          . basename( $segments )
-          . '.php';
-
       // Search the matched block.
-      if( file_exists( $this->blockName ) ) {
-        // Handle middlwares.
-        $this->handleMiddleware($segments);
+      if( file_exists( $this->blockAbsolutePath ) ) {
+        // Handle middleware.
+        $this->handleMiddleware($this->segments);
         // A block file was found, load the block.
-        $this->loadBlock();
+        $this->blockHandler->load(
+          $this->blockAbsolutePath,
+          $this->segments,
+          $this->requestType,
+          $this->blockLabel
+        );
       } else {
         // No block was found, return not found error.
         \App::response()
@@ -158,52 +185,6 @@
           ->error()
           ->pretty()
           ->send();
-      }
-    }
-
-    /**
-     * Load the default block.
-     * @param   none
-     * @return  none
-     *
-     */
-    public function goToDefaultBlock() {
-      // Set default block path.
-      $this->blockName = '../blocks/index/get.index.php';
-      // Load the default block.
-      $this->loadBlock();
-    }
-
-    /**
-     * Load a block.
-     * @param   none
-     * @return  none
-     *
-     */
-    /* public function loadBlock() {
-      if( file_exists( $this->blockName ) ) {
-        // The block exists.
-        require_once $this->blockName;
-      } else {
-        // No block was found, return not found error.
-        \App::response()->data( 'Block not found.' )->error()->pretty()->send();
-      }
-    } */
-
-    public function loadBlock () {
-      if( file_exists( $this->blockName ) ) {
-        require_once $this->blockName;
-        $blockObject = new \ZincPHP\Blocks\index\post\PostBlock();
-        $blockObject->onMount();
-        $blockObject->onUnMount();
-        $blockObject->middleware();
-        $blockObject->validation();
-        $blockObject->library();
-        $blockObject->response();
-        // $this->endApp();
-      } else {
-        // No block was found, return not found error.
-        \App::response()->data( 'Block not found.' )->error()->pretty()->send();
       }
     }
 
