1e164ab36eee07202e83bc39327316b9e3dfec45
Kazi Mehedi Hasan <rakibtg@gmail.com>
2019-02-18 11:27:51 +0600

    block handler for router

---
+++ app/core/router/HandleBlockTraits.php
@@ -0,0 +1,77 @@
+<?php
+  namespace ZincPHP\Route;
+
+  trait HandleBlockTraits {
+
+    /**
+     * Generate the namespace of the current block.
+     * 
+     * @param   none
+     * @return  string  The block namespace.
+     *
+     */
+    private function getBlockNamespace() {
+      return '\\ZincPHP\\Blocks' 
+        . str_replace('/', '\\', $this->segments)
+        . '\\'
+        . ucfirst($this->requestType)
+        . ucfirst($this->blockLabel);
+    }
+
+    /**
+     * Load a block class and then instantiate it.
+     * This will also maintain the lif-cycle 
+     * of the block.
+     * 
+     * @return  void
+     */
+
+    public function handleBlock() {
+      $_blockNamespace = $this->getBlockNamespace();
+      require_once $this->blockAbsolutePath;
+      $blockObject = new $_blockNamespace();
+
+      // Handle middleware for this block.
+      $this->handleMiddleware(
+        $blockObject->middleware()
+      );
+
+      // Mount the onMount life cycle class of the block.
+      try {
+        $blockObject->onMount();
+      } catch (Exception $e) {
+        \App::exception( $e );
+      }      
+
+      $blockObject->validation();
+      $blockObject->library();
+      $blockObject->response()->send();
+
+      // Mount the onUnMount life cycle class of the block.
+      try {
+        $blockObject->onUnMount();
+      } catch (Exception $e) {
+        \App::exception( $e );
+      }
+    }
+
+    /**
+     * Go to the block based on its request type.
+     *
+     * @param     none
+     * @return    none
+     */
+    public function goToCurrentBlock() {
+      if( file_exists( $this->blockAbsolutePath ) ) {
+        $this->handleBlock();
+      } else {
+        // No block was found!
+        \App::response()
+          ->data( 'Block not found.' )
+          ->error()
+          ->pretty()
+          ->send();
+      }
+    }
+
+  }
