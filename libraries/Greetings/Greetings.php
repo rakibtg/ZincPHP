ffe42ea0e44fd7538f04a37f7fd4d9189513357a
rakibtg <rakibtg@gmail.com>
2019-02-18 23:13:10 +0600

    test library

---
+++ libraries/Greetings/Greetings.php
@@ -0,0 +1,16 @@
+<?php
+
+  namespace Greetings;
+
+  /**
+   * Describe what your library class would do.
+   *
+   */
+
+  class Greetings {
+
+    function hello() {
+      return 'hello world from greetings library';
+    }
+
+  }
