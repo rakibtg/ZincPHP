13ab2464ced6bd859efe22cbca0c420246975f12
rakibtg <rakibtg@gmail.com>
2019-02-19 00:12:58 +0600

    updated default block message

--- blocks/index/get.index.php
+++ blocks/index/get.index.php
@@ -1,17 +1,15 @@
 <?php
+  namespace ZincPHP\Blocks\index;
 
-  /**
-   * A sample greeting block.
-   * 
-   * @link https://getzincphp.github.io/docs/latest/Blocks
-   */
+  class GetIndex extends \ZincPHP\block\ZincBlock {
 
-  $greetings = [
-    'greetings' => 'Welcome to ZincPHP',
-    'to_do' => 'To edit this response, go to `get.index.php` inside `blocks` directory'
-  ];
+    public function response() {
+      $greetings = [
+        'greetings' => 'Welcome to ZincPHP',
+        'to_do' => 'To edit this response, go to `get.index.php` inside `blocks` directory'
+      ];
 
-  \App::response()
-    ->data( $greetings )
-    ->pretty()
-    ->send();
+      return \App::response($greetings)
+        ->pretty();
+    }
+  }
